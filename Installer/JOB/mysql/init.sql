11aa4ece0c2a97368454dd478548742858243a3b
lijinfeng2011 <lijinfeng2011@gmail.com>
2020-12-05 12:54:09 +0800

    Give open-c3 administrator permission

--- Installer/JOB/mysql/init.sql
+++ Installer/JOB/mysql/init.sql
@@ -1,6 +1,9 @@
 use connector;
 insert into userinfo (`name`,`pass`)values('open-c3','4cb9c8a8048fd02294477fcb1a41191a');
+insert into userauth(name,level)values('open-c3',3);
+
 insert into tree(id,name,len)values(10,'open-c3.ops.opsdev.c3_demo',4);
 insert into tree(id,name,len)values(9,'open-c3.ops.opsdev',3);
 insert into tree(id,name,len)values(8,'open-c3.ops',2);
 insert into tree(id,name,len)values(7,'open-c3',1);
+
