f792ea1c0d4c08cebef3fec7d1d582a44cfd62ac
lijinfeng2011 <lijinfeng2011@gmail.com>
2020-12-12 18:30:45 +0800

    Add open-c3.conf

--- Installer/cluster/deploy/local/c3-front.pl
+++ Installer/cluster/deploy/local/c3-front.pl
@@ -23,4 +23,5 @@ unless( -e "$MYDan::PATH/c3-front" )
 
 die "rsync fail: $!" if system "rsync -av $MYDan::PATH/PKG/c3-front-$o{version}/ $MYDan::PATH/c3-front/dist/ --delete";
 die "copy open-c3.org.conf fail: $!" if system "cp $MYDan::PATH/PKG/c3-front-$o{version}/nginxconf/open-c3.org.conf /etc/nginx/conf.d/";
+die "copy open-c3.conf fail: $!" if system "cp $MYDan::PATH/PKG/c3-front-$o{version}/nginxconf/open-c3.conf /etc/nginx/conf.d/";
 die "reload nginx fail: $!"   if system "nginx -s reload";
