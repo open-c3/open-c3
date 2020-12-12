46729db512a2aa46d4af35c82e743c7bd847ea6d
lijinfeng2011 <lijinfeng2011@gmail.com>
2020-12-12 22:16:57 +0800

    The database container is named open-c3-database

--- Installer/cluster/deploy/local/c3-database.pl
+++ Installer/cluster/deploy/local/c3-database.pl
@@ -18,4 +18,4 @@ $option->assert( qw( username password  ) );
 
 my $e = $o{username} eq 'root' ? '' : "-e MYSQL_USER='$o{username}' -e MYSQL_PASS='$o{password}'";
 
-die "start fail: $!" if system "docker run -itd --restart=always -e MYSQL_ROOT_PASSWORD='$o{password}' $e -v $MYDan::PATH/Installer/C3/mysql/init/:/docker-entrypoint-initdb.d/ -v /data/open-c3-data/mysql-data:/var/lib/mysql -v $MYDan::PATH/Installer/C3/mysql/conf/my.cnf:/etc/my.cnf -p 3306:3306 mysql:5.5";
+die "start fail: $!" if system "docker run -itd --restart=always --name open-c3-database -e MYSQL_ROOT_PASSWORD='$o{password}' $e -v $MYDan::PATH/Installer/C3/mysql/init/:/docker-entrypoint-initdb.d/ -v /data/open-c3-data/mysql-data:/var/lib/mysql -v $MYDan::PATH/Installer/C3/mysql/conf/my.cnf:/etc/my.cnf -p 3306:3306 mysql:5.5";
