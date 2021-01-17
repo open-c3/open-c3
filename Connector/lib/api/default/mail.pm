5bafdcb915c4dbdc3597f602b0d37a765853e299
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-01-17 22:46:37 +0800

    Fix: email and sms fail about approve

--- Connector/lib/api/default/mail.pm
+++ Connector/lib/api/default/mail.pm
@@ -1,25 +1,17 @@
 package api::default::mail;
 use Dancer ':syntax';
 use Dancer qw(cookie);
-use Encode qw(encode);
 
 use JSON;
 use POSIX;
-use MIME::Base64;
 use api;
 use uuid;
 use Format;
-use Digest::MD5;
 
 get '/default/mail' => sub {
     my ( $ssocheck, $ssouser ) = api::ssocheck(); return $ssocheck if $ssocheck;
 
     my $mesg = eval{ $api::mysql->query( "select time,title,content from `usermail` where user = '$ssouser' order by id desc limit 100", [ 'time', 'title', 'content' ] ) };
-
-    map{
-        $_->{title} = decode_base64( $_->{title});
-        $_->{content} = decode_base64( $_->{content});
-    }@$mesg;
 
     return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => $mesg };
 };
@@ -35,10 +27,7 @@ post '/default/mail' => sub {
     )->check( %$param );
     return  +{ stat => $JSON::false, info => "check format fail $error" } if $error;
 
-    my $title = encode_base64( $param->{title} );
-    my $content = encode_base64( $param->{content} );
-
-    eval{ $api::mysql->execute( "insert into usermail (`user`,`title`,`content`) values('$param->{user}','$title','$content')" ); };
+    eval{ $api::mysql->execute( "insert into usermail (`user`,`title`,`content`) values('$param->{user}','$param->{title}','$param->{content}')" ); };
     return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, info => 'ok' };
 };
 
--- Connector/lib/api/default/mesg.pm
+++ Connector/lib/api/default/mesg.pm
@@ -1,21 +1,17 @@
 package api::default::mesg;
 use Dancer ':syntax';
 use Dancer qw(cookie);
-use Encode qw(encode);
 
 use JSON;
 use POSIX;
-use MIME::Base64;
 use api;
 use uuid;
 use Format;
-use Digest::MD5;
 
 get '/default/mesg' => sub {
     my ( $ssocheck, $ssouser ) = api::ssocheck(); return $ssocheck if $ssocheck;
 
     my $mesg = eval{ $api::mysql->query( "select time,mesg from `usermesg` where user = '$ssouser' order by id desc limit 100", [ 'time', 'mesg' ] ) };
-    map{ $_->{mesg} = decode_base64( $_->{mesg}); }@$mesg;
 
     return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => $mesg };
 };
@@ -30,9 +26,7 @@ post '/default/mesg' => sub {
     )->check( %$param );
     return  +{ stat => $JSON::false, info => "check format fail $error" } if $error;
 
-    my $mesg = encode_base64( $param->{mesg} );
-
-    eval{ $api::mysql->execute( "insert into usermesg (`user`,`mesg`) values('$param->{user}','$mesg')" ); };
+    eval{ $api::mysql->execute( "insert into usermesg (`user`,`mesg`) values('$param->{user}','$param->{mesg}')" ); };
     return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, info => 'ok' };
 };
 
