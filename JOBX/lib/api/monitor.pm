6a4f94a4f70f2f2db9d61a40c48baf2e1b20a0d5
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-04-03 12:51:05 +0800

    添加接口给外部监控进行监控

--- JOBX/lib/api/monitor.pm
+++ JOBX/lib/api/monitor.pm
@@ -7,7 +7,20 @@ use POSIX;
 use api;
 
 get '/monitor' => sub {
-    my $pmscheck = api::pmscheck( 'openc3_jobx_read', 0 ); return $pmscheck if $pmscheck;
+    my @col = qw( id time time_s stat host type key val );
+    my $r = eval{ 
+        $api::mysql->query( 
+            sprintf( "select %s from `monitor`", join( ',', map{"`$_`"}@col ) ), \@col
+        )};
+
+    my $timeout = time - 90;
+    map{ $_->{stat} = 'timeout' if $_->{time_s} < $timeout }@$r;
+
+    return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => $r };
+};
+
+get '/monitor/metrics' => sub {
+    header( 'Content-Type' => 'text/plain' );
 
     my @col = qw( id time time_s stat host type key val );
     my $r = eval{ 
@@ -15,7 +28,10 @@ get '/monitor' => sub {
             sprintf( "select %s from `monitor`", join( ',', map{"`$_`"}@col ) ), \@col
         )};
 
-    return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => $r };
+    my $timeout = time - 90;
+    map{ $_->{val} = 0 if $_->{time_s} < $timeout }@$r;
+
+    return join "", map{ "$_->{key}\{host=\"$_->{host}\",type=\"$_->{type}\"\} $_->{val}\n" }@$r;
 };
 
 true;
