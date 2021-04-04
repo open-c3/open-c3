c091762919e18515d1a094646001795e97a5d96b
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-04-04 12:28:43 +0800

    添加mysql指标监控

--- JOBX/lib/api/monitor.pm
+++ JOBX/lib/api/monitor.pm
@@ -44,4 +44,26 @@ get '/monitor/metrics' => sub {
     }
 };
 
+get '/monitor/metrics/mysql' => sub {
+    my $param = params();
+
+    my $r = eval{ $api::mysql->query( "SHOW GLOBAL STATUS" )};
+    my %r = map{ $_->[0] => $_->[1] }@$r;
+
+    map{
+        return +{ stat => $JSON::false, info => "nofind $_" } unless defined $r{$_};
+    }qw( Com_select Com_insert Com_update Com_delete Slow_queries Threads_connected Innodb_buffer_pool_pages_total Innodb_buffer_pool_pages_free );
+
+    my %re;
+
+    $re{read} = $r{Com_select};
+    $re{write} = $r{Com_insert} + $r{Com_update} + $r{Com_delete};
+
+    $re{slow_queries} = $r{Slow_queries};
+    $re{threads_connected} = $r{Threads_connected};
+    $re{buffer_pool_use} = int( 100 * ( $r{Innodb_buffer_pool_pages_total} - $r{Innodb_buffer_pool_pages_free} ) / $r{Innodb_buffer_pool_pages_total} );
+
+    return +{ stat => $JSON::true, data => \%re };
+};
+
 true;
