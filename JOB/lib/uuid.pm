50a06822a7aea24fbc51fd2770e3eef1f76eec21
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-04-20 11:10:36 +0800

    CI在expandHistory时候和JOB、JOBX联动

--- JOB/lib/uuid.pm
+++ JOB/lib/uuid.pm
@@ -8,6 +8,7 @@ sub new
     my ( $class, %this ) = @_;
 
     $this{chars} ||= [ "A" .. "Z", "a" .. "z", 0 .. 9 ];
+    $this{end} ||= [ "a" .. "z" ];
     $this{length} ||= 12;
     bless \%this, ref $class || $class;
 }
@@ -16,9 +17,8 @@ sub create_str
 {
     my $this = shift;
 
-    my ( $chars, $length ) = @$this{qw( chars length )};
-    join("", @$chars[ map { rand @$chars } ( 1 .. $length ) ]);
-
+    my ( $chars, $end, $length ) = @$this{qw( chars end length )};
+    join("", @$chars[ map { rand @$chars } ( 1 .. $length - 1 ) ]).$end->[rand @$end];
 }
 
 1;
