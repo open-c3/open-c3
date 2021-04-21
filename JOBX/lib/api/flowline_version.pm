b0656b10f921f7bee392ca31b7276c7104f2e386
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-04-21 20:16:23 +0800

    添加接口查询流水线发布的最新版本

---
+++ JOBX/lib/api/flowline_version.pm
@@ -0,0 +1,28 @@
+package api::flowline_version;
+use Dancer ':syntax';
+use Dancer qw(cookie);
+use Encode qw(encode);
+use JSON;
+use POSIX;
+use MIME::Base64;
+use api;
+use Format;
+use Util;
+use uuid;
+
+get '/flowline_version/:flowlineid' => sub {
+    my $param = params();
+    my $error = Format->new( flowlineid => qr/^\d+$/, 1,)->check( %$param );
+
+    return  +{ stat => $JSON::false, info => "check format fail $error" } if $error;
+
+    my @col = qw( jobxuuid version create_time );
+    my $r = eval{ 
+        $api::mysql->query( 
+            sprintf( "select %s from `flowline_version` where flowlineid='$param->{flowlineid}'", join( ',', @col ) ), \@col
+        )};
+
+    return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => @$r ? $r->[0] : +{} };
+};
+
+true;
