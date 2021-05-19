9db5e256e72da86b19ad16009de69c899441befb
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-05-19 10:55:12 +0800

    升级数据库前先自动备份

--- Installer/upgrade/v2.0.0/update.sh
+++ Installer/upgrade/v2.0.0/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/data/open-c3/Installer/scripts/databasectrl.sh  backup
+
 mysql="docker exec -i openc3-mysql mysql -uroot -popenc3123456^!"
 
 cat /data/open-c3/Installer/upgrade/v2.0.0/cleantemp.sql  | $mysql
