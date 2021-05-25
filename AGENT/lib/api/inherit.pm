b4c813fe52e6dfd684d938fba6531061e7b71e67
786967951 <786967951@qq.com>
2021-05-25 17:57:45 +0800

    审批内容可以使用变量

---
+++ AGENT/lib/api/inherit.pm
@@ -0,0 +1,27 @@
+package api::inherit;
+use Dancer ':syntax';
+use Dancer qw(cookie);
+use Encode qw(encode);
+
+use JSON;
+use POSIX;
+use MIME::Base64;
+use api;
+use Format;
+
+get '/inherit/:projectid' => sub {
+    my $param = params();
+    my $error = Format->new( projectid => qr/^\d+$/, 1 )->check( %$param );
+    return  +{ stat => $JSON::false, info => "check format fail $error" } if $error;
+
+    my $pmscheck = api::pmscheck( 'openc3_agent_read', $param->{projectid} ); return $pmscheck if $pmscheck;
+
+    my @col = qw( id projectid inheritid create_time fullname);
+    my $r = eval{ 
+        $api::mysql->query( 
+            sprintf( "select %s from openc3_agent_inherit where projectid='$param->{projectid}'", join( ',', @col)), \@col )};
+
+    return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => @$r ? $r->[0] : +{} };
+};
+
+true;
