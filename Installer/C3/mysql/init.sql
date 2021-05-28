744a0849541fafb4ccbfc6dcd71725b8833f3e19
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-05-28 20:01:36 +0800

    添加private节点的数据库初始化

--- Installer/C3/mysql/init.sql
+++ Installer/C3/mysql/init.sql
@@ -7,3 +7,5 @@ insert into openc3_connector_tree(id,name,len)values(9,'open-c3.ops.opsdev',3);
 insert into openc3_connector_tree(id,name,len)values(8,'open-c3.ops',2);
 insert into openc3_connector_tree(id,name,len)values(7,'open-c3',1);
 
+insert into openc3_connector_private(id,user)values('4000000001','open-c3');
+delete from openc3_connector_private;
