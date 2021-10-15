cf0d63a677420078c41c09b388c183c0ca665021
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-10-15 19:34:37 +0800

    添加工具管理prometheus

---
+++ prometheus/bin/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+X=$(docker inspect  openc3-prometheus 2>&1|grep Created|wc -l)
+
+mkdir -p /data/prometheus-data
+chmod 777 /data/prometheus-data
+
+if [ ! -f /data/open-c3/prometheus/config/prometheus.yml ];then
+    cp /data/open-c3/prometheus/config/prometheus.example.yml /data/open-c3/prometheus/config/prometheus.yml
+fi
+
+if [ ! -f /data/open-c3/prometheus/config/openc3_node_sd.json ];then
+    cp /data/open-c3/prometheus/config/openc3_node_sd.example.json /data/open-c3/prometheus/config/openc3_node_sd.json
+fi
+
+if [ "X1" == "X$X"  ]; then
+    docker start openc3-prometheus
+else
+    docker run -d -p 9090:9090 -v /data/prometheus-data:/data/prometheus-data -v /data/open-c3/prometheus:/data/prometheus-root  --name openc3-prometheus prom/prometheus  --config.file  /data/prometheus-root/config/prometheus.yml --storage.tsdb.path=/data/prometheus-data
+fi
+
