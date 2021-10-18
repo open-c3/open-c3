b048c493a4f03f96e5394a02ce342e0b6f67df16
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-10-18 16:24:15 +0800

    prometheus启动时处理web.external-url参数

--- prometheus/bin/start.sh
+++ prometheus/bin/start.sh
@@ -13,9 +13,15 @@ if [ ! -f /data/open-c3/prometheus/config/openc3_node_sd.yml ];then
     cp /data/open-c3/prometheus/config/openc3_node_sd.example.yml /data/open-c3/prometheus/config/openc3_node_sd.yml
 fi
 
+Externalur=""
+MYIP=$(ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}'|grep -v ^172|head -n 1)
+if [ "X" != "X$MYIP" ];then
+   Externalur="--web.external-url=http://$MYIP:9090"
+fi
+
 if [ "X1" == "X$X"  ]; then
     docker start openc3-prometheus
 else
-    docker run -d -p 9090:9090 -v /data/prometheus-data:/data/prometheus-data -v /data/open-c3/prometheus:/data/prometheus-root  --name openc3-prometheus prom/prometheus  --config.file  /data/prometheus-root/config/prometheus.yml --storage.tsdb.path=/data/prometheus-data
+    docker run -d -p 9090:9090 -v /data/prometheus-data:/data/prometheus-data -v /data/open-c3/prometheus:/data/prometheus-root --name openc3-prometheus prom/prometheus --config.file /data/prometheus-root/config/prometheus.yml --storage.tsdb.path=/data/prometheus-data $Externalur
 fi
 
