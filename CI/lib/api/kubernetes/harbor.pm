9d772dae3280375945bba31d71bd6a0f9a0b6919
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-12-05 12:19:51 +0800

    添加接口返回harbor镜像列表

---
+++ CI/lib/api/kubernetes/harbor.pm
@@ -0,0 +1,42 @@
+package api::kubernetes::harbor;
+use Dancer ':syntax';
+use Dancer qw(cookie);
+use Encode qw(encode);
+use FindBin qw( $RealBin );
+use JSON;
+use POSIX;
+use api;
+use Format;
+use MIME::Base64;
+use Time::Local;
+use File::Temp;
+use api::kubernetes;
+
+our %handle = %api::kubernetes::handle;
+
+get '/kubernetes/harbor/repository' => sub {
+    my $param = params();
+    my $error = Format->new( 
+        ticketid => qr/^\d+$/, 1,
+    )->check( %$param );
+
+    return  +{ stat => $JSON::false, info => "check format fail $error" } if $error;
+    my $pmscheck = api::pmscheck( 'openc3_ci_read', 0 ); return $pmscheck if $pmscheck;
+
+    my ( $cmd, $handle ) = ( "/data/Software/mydan/CI/bin/harbor-searchimage $param->{ticketid} 2>/dev/null", 'getsearchharborimage' );
+    return +{ stat => $JSON::true, data => +{ kubecmd => $cmd, handle => $handle }} if request->headers->{"openc3event"};
+
+    return &{$handle{$handle}}( `$cmd`//'', $? ); 
+};
+
+$handle{getsearchharborimage} = sub
+{
+    my ( $x, $status ) = @_;
+    return +{ stat => $JSON::false, data => $x } if $status;
+
+    my $data = eval{ YAML::XS::Load $x };
+    return +{ stat => $JSON::false, info => $@ } if $@;
+    return +{ stat => $JSON::true, data => $data };
+};
+
+true;
