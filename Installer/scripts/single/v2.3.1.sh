b71971cad5675a22754c080b24738b7ed42433db
lijinfeng2011 <lijinfeng2011@gmail.com>
2021-12-09 22:42:58 +0800

    添加v2.3.1版本初始化脚本

---
+++ Installer/scripts/single/v2.3.1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker exec -it c3_openc3-server_1 /data/Software/mydan/perl/bin/cpan install AnyEvent::HTTPD::Router AnyEvent::HTTPD::CookiePatch
+
+docker cp /data/open-c3/Installer/install-cache/bin/kubectl  c3_openc3-server_1:/usr/bin/
+docker cp /data/open-c3/Installer/install-cache/bin/yaml2json  c3_openc3-server_1:/usr/bin/
+docker cp /data/open-c3/Installer/install-cache/bin/json2yaml  c3_openc3-server_1:/usr/bin/
