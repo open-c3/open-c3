116a1b384384ab4c747317a11310a43152f10a5c
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-01-26 16:24:59 +0800

    lua权限验证，根据http的方法不一样做不同的权限检查

--- lua/config/lua/sso.example.lua
+++ lua/config/lua/sso.example.lua
@@ -4,25 +4,51 @@ local json    = require("cjson")
 local httpc = http.new()
 
 if ngx.var.cookie_sid then
-  local res, err = httpc:request_uri(
-      "http://OPENC3_SERVER_IP:88/api/connector/connectorx/username?cookie=" .. ngx.var.cookie_sid,
-      {
-          method = "GET",
-          keepalive = false
-      }
-  )
+    if ngx.var.request_method == "GET" then
 
-  ngx.log(ngx.ERR, "SSO Connect!, cookie:[", ngx.var.cookie_sid,"]")
+        local res, err = httpc:request_uri(
+            "http://OPENC3_SERVER_IP:88/api/connector/connectorx/username?cookie=" .. ngx.var.cookie_sid,
+            {
+                method = "GET",
+                keepalive = false
+            }
+        )
 
-  local ret = json.decode(res.body)
-  if ret then
-      if ret.data and ret.stat == true and ret.data.user and ret.data.user ~= ngx.null then
-           ngx.log(ngx.ERR, "SSO Connect Faild!.cookie:", ngx.var.cookie_sid, "], user:[",ret.data.user ,"]")
-          return
-      end
-  else
-      ngx.log(ngx.ERR, "SSO Connect Faild!.cookie:", ngx.var.cookie_sid, "]")
-  end
+        ngx.log(ngx.ERR, "SSO Connect!, cookie:[", ngx.var.cookie_sid,"]")
+
+        local ret = json.decode(res.body)
+        if ret then
+            if ret.data and ret.stat == true and ret.data.user and ret.data.user ~= ngx.null then
+                 ngx.log(ngx.ERR, "SSO Connect Faild!.cookie:", ngx.var.cookie_sid, "], user:[",ret.data.user ,"]")
+                return
+            end
+        else
+            ngx.log(ngx.ERR, "SSO Connect Faild!.cookie:", ngx.var.cookie_sid, "]")
+        end
+
+    else
+        local res, err = httpc:request_uri(
+            "http://OPENC3_SERVER_IP:88/api/connector/connectorx/point?point=openc3_job_root&cookie=" .. ngx.var.cookie_sid,
+            {
+                method = "GET",
+                keepalive = false
+            }
+        )
+
+        ngx.log(ngx.ERR, "PMS Connect!, cookie:[", ngx.var.cookie_sid,"]")
+
+        local ret = json.decode(res.body)
+        if ret then
+            if ret.data and ret.stat == true and ret.data == 1 then
+                 ngx.log(ngx.ERR, "PMS Connect Faild!.cookie:", ngx.var.cookie_sid, "], user:[",ret.data ,"]")
+                return
+            end
+        else
+            ngx.log(ngx.ERR, "PMS Connect Faild!.cookie:", ngx.var.cookie_sid, "]")
+        end
+
+    end
+
 end
 
 ngx.log(ngx.ERR, "SSO Blocked!cookie:[", ngx.var.cookie_sid, "]")
