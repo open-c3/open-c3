b2fefb2feaac5457df9c0b60e20922864a667173
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-01-30 00:20:16 +0800

    升级monitor数据生成服务，进行统一模块处理

---
+++ Connector/lib/OPENC3/PP/Server.pm
@@ -0,0 +1,80 @@
+package OPENC3::PP::Server;
+
+use warnings;
+use strict;
+
+sub new
+{
+    my ( $class, %this ) = @_;
+
+    $0 = $this{name} if $this{name};
+    $this{interval} ||= 60;
+    $this{timeout} ||= 120;
+
+    bless \%this, ref $class || $class;
+}
+
+sub run
+{
+    my ( $this, @cmd ) = @_;
+    die unless @cmd;
+    my ( $interval, $timeout ) = @$this{qw( interval timeout )};
+
+    $ENV{PATH} .= sprintf ":%s", join ":", map{ "/data/Software/mydan/$_/pp" }qw( Connector JOB JOBX AGENT CI );
+
+    warn "[warn]nofind pkill" if system "pkill --help 1>/dev/null";
+
+    warn ">> start\n";
+
+    while(1)
+    {
+
+        my $time = time;
+        eval{
+            my $pid;
+            local $SIG{ALRM} = sub{
+
+                if( $pid )
+                {
+                    system "pkill -15 -P $pid";
+                    kill 'TERM', $pid;
+                }
+
+                warn "timeout alarm, pid:". $pid
+            };
+
+            alarm $timeout;
+
+            for my $cmd ( @cmd )
+            {
+                unless( $pid = fork )
+                {
+                    exec $cmd;
+                }
+                else
+                {
+                    wait;
+                    die if $?;
+                }
+
+            }
+
+            alarm 0;
+        };
+
+        warn "run fail: $@" if $@;
+
+        my $due = $interval + $time - time;
+        if( $due > 0 )
+        {
+            sleep $due;
+        }
+        else
+        {
+            warn "timeout $due.\n";
+        }
+    }
+
+}
+
+1;
