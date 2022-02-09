064d6d7095c718b868f84ef9d33902b94441666e
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-02-09 18:32:33 +0800

    init.sql处理private节点id问题

--- Installer/C3/mysql/init.sql
+++ Installer/C3/mysql/init.sql
@@ -8,4 +8,3 @@ insert into openc3_connector_tree(id,name,len)values(8,'open-c3.ops',2);
 insert into openc3_connector_tree(id,name,len)values(7,'open-c3',1);
 
 insert into openc3_connector_private(id,user)values('4000000001','open-c3');
-delete from openc3_connector_private;
