3f840905a0f33f2eaa2b4e6dc9ff43491bc4e707
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-02-19 16:55:44 +0800

    调整监控数据库表结构

--- Installer/scripts/single/v2.5.1.sql
+++ Installer/scripts/single/v2.5.1.sql
@@ -17,12 +17,16 @@ create table `openc3_monitor_config_rule`(
 `id`            int(16) unsigned not null primary key auto_increment comment 'id',
 `projectid` int(16) unsigned comment '项目id',
 `alert` VARCHAR(100) comment 'alert',
-`expr` VARCHAR(100) comment 'expr',
+`expr` VARCHAR(3000) comment 'expr',
 `for` VARCHAR(100) comment 'for',
 `severity` VARCHAR(100) comment 'severity',
-`summary` VARCHAR(100) comment 'summary',
-`description` VARCHAR(100) comment 'description',
+`summary` VARCHAR(1000) comment 'summary',
+`description` VARCHAR(1000) comment 'description',
 `value` VARCHAR(100) comment 'value',
+`model` VARCHAR(100) comment 'model',#simple,custom
+`metrics` VARCHAR(100) comment 'metrics',
+`method` VARCHAR(20) comment 'method',#> < >= <=
+`threshold` VARCHAR(100) comment 'threshold',
 `edit_user` VARCHAR(100) comment '编辑者',
 `edit_time` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP comment '创建时间'
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='monitorconfigrule';
@@ -34,6 +38,9 @@ create table `openc3_monitor_config_user`(
 `edit_user` VARCHAR(100) comment '编辑者',
 `edit_time` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP comment '创建时间'
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 comment='monitorconfiguser';
+
+
+#alter table openc3_monitor_config_rule modify column `summary` varchar(1000) DEFAULT NULL COMMENT 'summary';
 
 ### 数据库回滚
 
