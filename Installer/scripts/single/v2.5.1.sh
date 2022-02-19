19cc2946cd1eb8062a2d4374f5a7d87e7d8340bd
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-02-19 16:43:42 +0800

    添加aws_c3工具

--- Installer/scripts/single/v2.5.1.sh
+++ Installer/scripts/single/v2.5.1.sh
@@ -9,3 +9,5 @@ docker cp /data/open-c3/Installer/install-cache/bin/json2yaml  openc3-server:/us
 #TODO 集群发布后，webshell容器需要拷贝kubectl命令到容器中，否则影响webshell进入pod。
 #手动创建一个流水线，作为kubernetes管理的流水线默认模版。
 #升级的时候可能会报错，确认CI模块的配置里面的端口是否被正确能让api.event生效。
+
+docker cp /data/open-c3/CI/bin/aws_c3 openc3-server:/usr/local/bin/
