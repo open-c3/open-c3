da5f09bd363170d2432832616ddc6a9b669e5268
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-03-15 11:30:07 +0800

    私有节点管理中添加消息通道的测试

--- c3-front/src/app/pages/global/private/private.controller.js
+++ c3-front/src/app/pages/global/private/private.controller.js
@@ -45,6 +45,21 @@
             });
         };
 
+         vm.testsend = function ( user ) {
+            $http.post('/api/agent/monitor/config/usertest', { projectid: vm.treeid, 'user': user}).then(
+                function successCallback(response) {
+                    if (response.data.stat){
+                        swal({title: "消息发送已经提交！", text: "确认是否收到消息", type: 'success'});
+                    }else {
+                        toastr.error( "发送消息失败：" + response.data.info );
+                    }
+                },
+                function errorCallback (response ){
+                    toastr.error( "发送消息添加失败：" + response.status );
+                }
+            );
+        };
+
     }
 
 })();
