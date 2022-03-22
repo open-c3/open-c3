b608cb58dc8ba298586a12ede18b685edc0fefe1
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-03-22 17:16:52 +0800

    添加v2.5.9初始化脚本

---
+++ Installer/scripts/single/v2.5.9.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker exec -i openc3-server /data/Software/mydan/perl/bin/cpan install AnyEvent::HTTPD::Router AnyEvent::HTTPD::CookiePatch AnyEvent::HTTP
+
+docker cp /data/open-c3/Installer/install-cache/bin/kubectl  openc3-server:/usr/bin/
+docker cp /data/open-c3/Installer/install-cache/bin/yaml2json  openc3-server:/usr/bin/
+docker cp /data/open-c3/Installer/install-cache/bin/json2yaml  openc3-server:/usr/bin/
+
+#TODO 集群发布后，webshell容器需要拷贝kubectl命令到容器中，否则影响webshell进入pod。
+#手动创建一个流水线，作为kubernetes管理的流水线默认模版。
+#升级的时候可能会报错，确认CI模块的配置里面的端口是否被正确能让api.event生效。
+
+docker cp /data/open-c3/CI/bin/aws_c3 openc3-server:/usr/local/bin/
+
+docker cp /data/open-c3/lua/lualib/resty/http.lua openc3-lua:/usr/local/openresty/lualib/resty/http.lua
+docker cp /data/open-c3/lua/lualib/resty/http_connect.lua openc3-lua:/usr/local/openresty/lualib/resty/http_connect.lua
+docker cp /data/open-c3/lua/lualib/resty/http_headers.lua openc3-lua:/usr/local/openresty/lualib/resty/http_headers.lua
+
+#oncall 需要的插件
+docker exec -i openc3-server /data/Software/mydan/perl/bin/cpan install DateTime
+mkdir -p /data/open-c3-data/glusterfs/oncall/{conf,data}
+
+#mail mon
+docker exec -i openc3-server /data/Software/mydan/perl/bin/cpan install Mail::POP3Client Email::MIME Email::MIME::RFC2047::Decoder
+mkdir -p /data/open-c3-data/glusterfs/mailmon/{conf,data,run}
