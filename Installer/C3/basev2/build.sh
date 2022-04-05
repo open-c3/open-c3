e4626b129b6810dfccd5ea929385c4f291e43251
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-04-05 11:31:40 +0800

    fix

--- Installer/C3/basev2/build.sh
+++ Installer/C3/basev2/build.sh
@@ -5,4 +5,4 @@ if [ "X$VERSION" == "X" ];then
     VERSION=$(date +%Y%m%d)
 fi
 echo VERSION:$VERSION
-time docker build . -t openc3/basev2:$VERSION -f dockerfile . --no-cache
+time docker build . -t openc3/basev2:$VERSION -f dockerfile --no-cache
