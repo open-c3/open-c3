4bc9abec507a7fa779b494fefe01ea4c462917b9
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-04-17 22:22:35 +0800

    添加sysctl

---
+++ Connector/lib/OPENC3/SysCtl.pm
@@ -0,0 +1,37 @@
+package OPENC3::SysCtl;
+
+use warnings;
+use strict;
+
+use YAML::XS;
+
+my $dconf = '/data/Software/mydan/Connector/config/sysctl.conf';
+my $pconf = '/data/open-c3-data/sysctl.conf';
+
+sub _load
+{
+    my $x = shift @_;
+    my $conf = eval{ YAML::XS::LoadFile $x };
+    die "load conf $x fail: $@" if $@;
+    die unless $conf && ref $conf eq 'HASH';
+    return $conf;
+}
+sub new
+{
+    my ( $class, %this ) = @_;
+    $this{data} = -f $pconf ? +{ _load( $dconf ), _load( $pconf )} : _load( $dconf );
+    bless \%this, ref $class || $class;
+}
+
+sub get
+{
+    my ( $this, $key ) = @_;
+    return $this->{data}{$key};
+}
+
+sub dump
+{
+    return shift->{data};
+}
+
+1;
