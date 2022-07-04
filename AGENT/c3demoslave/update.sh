07fc29d92b526c8dae632555ccff6fb69d27952d
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-07-04 21:29:47 +0800

    demoslave添加http访问

--- AGENT/c3demoslave/update.sh
+++ AGENT/c3demoslave/update.sh
@@ -17,10 +17,9 @@ function updatedb()
     | xargs -i{} c3mc-base-db-ins -t openc3_job_nodelist projectid $ID name {} inip {} exip {} create_user open-c3 edit_user open-c3  edit_time '2022-07-01 17:00:00' create_time '2022-07-01 17:00:00' 
 
 }
-
-updatedb 7 10
-updatedb 8 8
-updatedb 9 5
+updatedb 7  10
+updatedb 8  8
+updatedb 9  5
 updatedb 10 2
 
 function updateslave()
@@ -30,5 +29,20 @@ function updateslave()
     | awk '{print $NF}' \
     | xargs  -i{} docker exec {} bash -c 'curl -L $OPEN_C3_ADDR/api/scripts/installAgent.sh | bash ; curl -L $OPEN_C3_ADDR/api/scripts/installAgentMon.sh | bash'
 }
-
 updateslave
+
+function updatenginx()
+{
+    cp /data/Software/mydan/AGENT/c3demoslave/api.demoslave.open-c3.org.conf /etc/nginx/conf.d/
+    nginx -s reload
+}
+updatenginx
+
+function http()
+{
+    docker ps \
+    | grep c3demoslave_openc3-demo-slave \
+    | awk '{print $NF}' \
+    | xargs -i{} docker cp /data/Software/mydan/AGENT/c3demoslave/python.http {}:/opt/mydan/dan/bootstrap/exec/python.http
+}
+http
