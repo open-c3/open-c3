0dc5b9c741a984714cf2989acf0bcece8b892645
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-07-07 16:01:57 +0800

    u - 添加k8s命名空间复制的作业插件k8snscp

--- c3-front/src/app/pages/business/job/plugin/cmd.controller.js
+++ c3-front/src/app/pages/business/job/plugin/cmd.controller.js
@@ -314,6 +314,10 @@
                 {
                     vm.showmachinelist = 0;
                 }
+                if( cont.search(/^#!k8snscp\b/) == 0 )
+                {
+                    vm.showmachinelist = 0;
+                }
             }
         }
 
@@ -570,10 +574,16 @@
         vm.getProUser();
 
         vm.reloadticket = function(ticketid){
-            $http.get('/api/ci/ticket?type=JobBuildin&ticketid=' + ticketid ).success(function(data){
+            $http.get('/api/ci/ticket?ticketid=' + ticketid ).success(function(data){
                 if( data.stat)
                 {
-                    vm.ticketinfo = data.data;
+                    vm.ticketinfo = [];
+                    angular.forEach(data.data, function (data, index) {
+                        if( data.type === 'KubeConfig' || data.type === 'JobBuildin' )
+                        {
+                            vm.ticketinfo.push(data)
+                        }
+                    });
                     vm.ticketinfo.unshift({ id: '0', name: 'null' })
                 }
                 else
