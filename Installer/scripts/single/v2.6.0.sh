d3e7deaeeb2fe56335e961d505155c42a2056619
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-07-12 12:11:03 +0800

    c3mc-base-nodeinfo 使用cache默认保存

--- Installer/scripts/single/v2.6.0.sh
+++ Installer/scripts/single/v2.6.0.sh
@@ -49,3 +49,5 @@ fi
 docker exec -i openc3-server /data/Software/mydan/perl/bin/cpan install Net::LDAP
 
 docker exec -i openc3-server bash -c "curl -L http://openc3-srv-docker/api/scripts/installAgent.sh |OPEN_C3_ADDR=http://openc3-srv-docker bash"
+
+mkdir -p /data/open-c3-data/cache/nodeinfo
