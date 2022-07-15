dfc2caa4ee7debd503870c936486e225e7c8f69f
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-07-15 09:59:32 +0800

    调整connector 启停脚本

--- Connector/restart-open-c3.sh
+++ Connector/restart-open-c3.sh
@@ -20,25 +20,25 @@ if [ "X$1" == "Xstart" ];then
     CTRL=start
 fi
 
-cd /data/Software/mydan/CI/tools && ./SetEnv -e test
+cd /data/Software/mydan/CI/tools    && ./SetEnv -e test
 cd /data/Software/mydan/AGENT/tools && ./SetEnv -e test
-cd /data/Software/mydan/JOB/tools && ./SetEnv -e test
-cd /data/Software/mydan/JOBX/tools && ./SetEnv -e test
+cd /data/Software/mydan/JOB/tools   && ./SetEnv -e test
+cd /data/Software/mydan/JOBX/tools  && ./SetEnv -e test
 
 cd /data/Software/mydan/Connector/tools && ./SetEnv -e test
 cd /data/Software/mydan/Connector/tools && ./$CTRL
 
 sleep 10;
 
-cd /data/Software/mydan/CI/tools && ./SetEnv -e test
+cd /data/Software/mydan/CI/tools    && ./SetEnv -e test
 cd /data/Software/mydan/AGENT/tools && ./SetEnv -e test
-cd /data/Software/mydan/JOB/tools && ./SetEnv -e test
-cd /data/Software/mydan/JOBX/tools && ./SetEnv -e test
+cd /data/Software/mydan/JOB/tools   && ./SetEnv -e test
+cd /data/Software/mydan/JOBX/tools  && ./SetEnv -e test
 
-cd /data/Software/mydan/CI/tools && ./$CTRL
+cd /data/Software/mydan/CI/tools    && ./$CTRL
 cd /data/Software/mydan/AGENT/tools && ./$CTRL
-cd /data/Software/mydan/JOB/tools && ./$CTRL
-cd /data/Software/mydan/JOBX/tools && ./$CTRL
+cd /data/Software/mydan/JOB/tools   && ./$CTRL
+cd /data/Software/mydan/JOBX/tools  && ./$CTRL
 
 /data/Software/mydan/web-shell/tools/start
 
