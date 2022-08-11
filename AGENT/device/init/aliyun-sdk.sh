fb23742237cb2ac4e971b8ecb65cc30b997ebba2
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-08-11 18:30:01 +0800

    u - CMDB 添加阿里云rds的自动同步插件

--- AGENT/device/init/aliyun-sdk.sh
+++ AGENT/device/init/aliyun-sdk.sh
@@ -4,3 +4,4 @@ curl https://bootstrap.pypa.io/pip/2.7/get-pip.py  > get-pip.py
 python get-pip.py 
 pip install aliyun-python-sdk-core
 pip install aliyun-python-sdk-ecs
+pip install aliyun-python-sdk-rds
