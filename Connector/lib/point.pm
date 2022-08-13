c0a8da0197211ec63cae08e696cc1b9298387b04
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-08-13 12:23:15 +0800

    提取point的map到函数外部
    当前是全局权限点使用，服务树相关的权限点检查也需要使用到。这里把它提取到函数外部。

--- Connector/lib/point.pm
+++ Connector/lib/point.pm
@@ -2,44 +2,48 @@ package point;
 use warnings;
 use strict;
 
+my %pointG = (
+    openc3_job_root          => [          3 ],
+    openc3_jobx_root         => [          3 ],
+    openc3_ci_root           => [          3 ],
+    openc3_agent_root        => [          3 ],
+    openc3_connector_root    => [          3 ],
+);
+
+my %pointT = (
+    openc3_job_read          => [ 0, 1, 2, 3 ],
+    openc3_job_write         => [       2, 3 ],
+    openc3_job_delete        => [       2, 3 ],
+    openc3_job_vssh          => [       2, 3 ],
+    openc3_job_vsshnobody    => [    1, 2, 3 ],
+    openc3_job_control       => [    1, 2, 3 ],
+
+    openc3_jobx_read         => [ 0, 1, 2, 3 ],
+    openc3_jobx_write        => [       2, 3 ],
+    openc3_jobx_delete       => [       2, 3 ],
+    openc3_jobx_control      => [    1, 2, 3 ],
+
+    openc3_ci_read           => [ 0, 1, 2, 3 ],
+    openc3_ci_write          => [       2, 3 ],
+    openc3_ci_delete         => [       2, 3 ],
+    openc3_ci_control        => [    1, 2, 3 ],
+
+    openc3_agent_read        => [ 0, 1, 2, 3 ],
+    openc3_agent_write       => [       2, 3 ],
+    openc3_agent_delete      => [       2, 3 ],
+
+    openc3_connector_read    => [ 0, 1, 2, 3 ],
+    openc3_connector_write   => [       2, 3 ],
+    openc3_connector_delete  => [       2, 3 ],
+);
+
 sub point
 {
     my ( $db, $point , $treeid, $user ) = @_;
 
     #level
     #0: login 1: dev 2:ops 3:admin
-    my %level = 
-        (
-            openc3_job_read          => [ 0, 1, 2, 3 ],
-            openc3_job_write         => [       2, 3 ],
-            openc3_job_delete        => [       2, 3 ],
-            openc3_job_vssh          => [       2, 3 ],
-            openc3_job_vsshnobody    => [    1, 2, 3 ],
-            openc3_job_control       => [    1, 2, 3 ],
-            openc3_job_root          => [          3 ],
-
-            openc3_jobx_read         => [ 0, 1, 2, 3 ],
-            openc3_jobx_write        => [       2, 3 ],
-            openc3_jobx_delete       => [       2, 3 ],
-            openc3_jobx_control      => [    1, 2, 3 ],
-            openc3_jobx_root         => [          3 ],
-
-            openc3_ci_read           => [ 0, 1, 2, 3 ],
-            openc3_ci_write          => [       2, 3 ],
-            openc3_ci_delete         => [       2, 3 ],
-            openc3_ci_control        => [    1, 2, 3 ],
-            openc3_ci_root           => [          3 ],
-
-            openc3_agent_read        => [ 0, 1, 2, 3 ],
-            openc3_agent_write       => [       2, 3 ],
-            openc3_agent_delete      => [       2, 3 ],
-            openc3_agent_root        => [          3 ],
-
-            openc3_connector_read    => [ 0, 1, 2, 3 ],
-            openc3_connector_write   => [       2, 3 ],
-            openc3_connector_delete  => [       2, 3 ],
-            openc3_connector_root    => [          3 ],
-        );
+    my %level = ( %pointG, %pointT );
 
     my %l = map{ $_ => 1 }@{  $level{$point}  };
 
