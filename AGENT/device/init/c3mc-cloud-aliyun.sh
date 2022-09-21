1abcb3726aa051fc8bcd8dc034300a774145c1a6
leowu <wupengcn301@gmail.com>
2022-09-21 16:33:33 +0800

    添加阿里云slb资源获取

--- AGENT/device/init/c3mc-cloud-aliyun.sh
+++ AGENT/device/init/c3mc-cloud-aliyun.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pip install aliyun-python-sdk-core==2.13.28 aliyun-python-sdk-ecs aliyun-python-sdk-r-kvstore aliyun-python-sdk-rds
+pip install aliyun-python-sdk-core==2.13.28 aliyun-python-sdk-ecs \
+      aliyun-python-sdk-r-kvstore aliyun-python-sdk-rds aliyun-python-sdk-slb
