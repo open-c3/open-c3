9f7f0e614075d2ff1e7db30c84df53c22d94be25
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-09-23 16:50:02 +0800

    fix 修复监控插件的命令输出，最后输出为空时为正常状态。

--- AGENT/agent.mon/lib/OPENC3/MYDan/MonitorV3/NodeExporter/Collector.pm
+++ AGENT/agent.mon/lib/OPENC3/MYDan/MonitorV3/NodeExporter/Collector.pm
@@ -97,7 +97,6 @@ sub new
                         pid => $pid, cb => sub{
                             delete $proc{$type}{pid};
                             my $input;my $n = sysread $rdr, $input, 102400;
-                            return unless $n;
 
                             $input = "" unless $n;
                             $input = "$proc{$type}{out}$input";
@@ -136,7 +135,7 @@ sub new
         after => 1, 
         interval => 15,
         cb => sub { 
-            $this{prom}->set( 'node_exporter_version', 13 );
+            $this{prom}->set( 'node_exporter_version', 14 );
             $this{prom}->set( 'node_collector_error', $promeerror, +{ collector => 'node_exporter_prome' } ) if defined $promeerror;
             $promeerror = undef;           
         }
