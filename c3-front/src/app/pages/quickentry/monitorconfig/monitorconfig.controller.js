8ff853a86db6958944bae401be830092f8b02d70
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-10-11 11:02:42 +0800

    u - 监控配置页面中显示主机列表

--- c3-front/src/app/pages/quickentry/monitorconfig/monitorconfig.controller.js
+++ c3-front/src/app/pages/quickentry/monitorconfig/monitorconfig.controller.js
@@ -137,6 +137,21 @@
 
         vm.reloadRule();
 
+        vm.reloadNodeinfo = function(){
+            vm.loadoverNodeinfo = false;
+            $http.get('/api/agent/nodeinfo/' + vm.treeid ).success(function(data){
+                if(data.stat == true) 
+                { 
+                    vm.activeNodeinfoTable = new ngTableParams({count:20}, {counts:[],data:data.data.reverse()});
+                    vm.loadoverNodeinfo = true;
+                } else { 
+                    toastr.error( "加载Nodeinfo失败:" + data.info )
+                }
+            });
+        };
+
+        vm.reloadNodeinfo();
+
         vm.reloadAlert = function(){
             vm.loadoverAlert = false;
             $http.get('/api/agent/monitor/alert/' + vm.treeid + "?siteaddr=" + vm.siteaddr ).success(function(data){
