b251428a4d8939a139e5cbf5e8ea11939225f02a
leowu <wupengcn301@gmail.com>
2022-10-19 19:31:00 +0800

    安装华为sdk时保证huaweicloudsdkcore为最新版

--- AGENT/device/init/c3mc-cloud-huawei.sh
+++ AGENT/device/init/c3mc-cloud-huawei.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+pip3 uninstall -y huaweicloudsdkcore
+
 pip3 install huaweicloudsdkcore huaweicloudsdkdcs huaweicloudsdkrds \
              huaweicloudsdkdds huaweicloudsdkecs huaweicloudsdkelb \
              huaweicloudsdkgaussdbfornosql huaweicloudsdkevs
