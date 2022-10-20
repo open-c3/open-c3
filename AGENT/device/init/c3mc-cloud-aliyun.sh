6c9e9baa53e98168ee25572a46da8927325e6976
leowu <wupengcn301@gmail.com>
2022-10-20 11:20:08 +0800

    安装阿里云sdk时更新相关包

--- AGENT/device/init/c3mc-cloud-aliyun.sh
+++ AGENT/device/init/c3mc-cloud-aliyun.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+pip3 install pycrypto -U
+pip3 install pycryptodome -U
+
 pip3 install aliyun-python-sdk-core aliyun-python-sdk-ecs \
       aliyun-python-sdk-r-kvstore aliyun-python-sdk-rds aliyun-python-sdk-slb oss2
