f36761ac5cfcf463b789a97f3cad391aaf6e4513
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-10-26 21:20:14 +0800

    优化install-pip.sh

--- AGENT/device/init/install-pip.sh
+++ AGENT/device/init/install-pip.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd /tmp
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py -O get-pip.py
 python ./get-pip.py
 
 ln -fsn /usr/local/python3/bin/pip3.7 /usr/local/bin/pip3
