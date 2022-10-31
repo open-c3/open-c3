70eaec5cb5c0c399625749e057b6520f0966d36f
leowu <wupengcn301@gmail.com>
2022-10-31 16:38:57 +0800

    调整腾讯云ckafka、clb、cos、cvm、cbs等资源的同步逻辑

---
+++ Connector/lib/pp/c3mc_cloud_qcloud_cos_get_tag.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+
+
+class GetTag:
+    def __init__(self, access_id, access_key, region, bucket_name):
+        self.access_id = access_id
+        self.access_key = access_key
+        self.region = region
+        self.bucket_name = bucket_name
+        self.client = self.create_client()
+
+    def create_client(self):
+        config = CosConfig(Region=self.region, SecretId=self.access_id,
+                           SecretKey=self.access_key)  # 获取配置对象
+        client = CosS3Client(config)
+        return client
+
+    def list_tag(self):
+        try:
+            response = self.client.get_bucket_tagging(
+                Bucket=self.bucket_name
+            )
+            if "TagSet" in response:
+                return response["TagSet"]["Tag"]
+            return []
+        except:
+            return []
