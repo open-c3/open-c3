a07191449c1b4a002932915323d75dfabbafbedc
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-11-08 18:40:07 +0800

    工单系统后端代码编译通过docker进行编译

--- Connector/tt/trouble-ticketing/dev.sh
+++ Connector/tt/trouble-ticketing/dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
-./control  build
+docker run --rm -i \
+  --workdir /code \
+  -v /data/open-c3/Connector/tt/trouble-ticketing:/code \
+  -v /data/open-c3/Connector/tt/trouble-ticketing/tmp:/go golang:1.17.10 \
+   go build -o trouble-ticketing main.go
+
 docker  exec -it openc3-server bash -c "/data/Software/mydan/Connector/tt/trouble-ticketing/tdev.sh"
