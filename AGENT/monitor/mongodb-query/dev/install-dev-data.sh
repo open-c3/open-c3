18f3529b75f83bbfbb57f4874f076e1947cf0217
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-11-09 17:54:05 +0800

    CMDB添加mongodb demo数据

--- AGENT/monitor/mongodb-query/dev/install-dev-data.sh
+++ AGENT/monitor/mongodb-query/dev/install-dev-data.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-mkdir -p /data/open-c3-data/device/curr/database/monitor-mongodb-dev/
-rsync  -av /data/open-c3/AGENT/monitor/mongodb-query/dev/monitor-mongodb-dev/ /data/open-c3-data/device/curr/database/monitor-mongodb-dev/
+DIST=monitor-mongodb-dev
+if [ "X$1" == "Xdemo" ]; then
+    DIST=idc-mongodb
+
+    mkdir -p /data/open-c3-data/device/curr/auth/mongodb.auth
+    date > /data/open-c3-data/device/curr/auth/mongodb.auth/open-c3
+fi
+
+mkdir -p /data/open-c3-data/device/curr/database/$DIST/
+rsync  -av /data/open-c3/AGENT/monitor/mongodb-query/dev/monitor-mongodb-dev/ /data/open-c3-data/device/curr/database/$DIST/
