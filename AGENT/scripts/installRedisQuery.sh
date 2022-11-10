20d982f2666d8e13497b0d935df1af4563ef6fc2
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-11-10 22:57:24 +0800

    更新installRedisQuery.sh,使用最新镜像，解决redis tls问题

--- AGENT/scripts/installRedisQuery.sh
+++ AGENT/scripts/installRedisQuery.sh
@@ -2,7 +2,7 @@
 
 curl -L $OPEN_C3_ADDR/api/scripts/installQueryInit.sh | bash
 
-IMAGE=openc3/redis-query:e2211101
+IMAGE=openc3/redis-query:e2211102
 VPATH=redis-exporter-v3
 NAME=openc3-redis-query
 
