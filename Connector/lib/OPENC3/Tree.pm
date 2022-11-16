a64c7f300b83b11174bd65d69c09b53e28c6e8b7
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-11-16 15:12:04 +0800

    CMDB服务树信息进行前缀合并 #433

--- Connector/lib/OPENC3/Tree.pm
+++ Connector/lib/OPENC3/Tree.pm
@@ -22,4 +22,42 @@ sub compress
     return join ',', sort{ $tree{$a} <=> $tree{$b} }keys %tree;
 }
 
+sub merge
+{
+    my $tree = join ',', @_;
+    my %tree = map{ $_ => 1 }grep{ $_ }split /,/, $tree;
+    return $tree unless 1 < keys %tree;
+
+    my %cnt;
+    for my $name ( keys %tree )
+    {
+        my @name = split /\./, $name;
+        pop @name;
+        while( @name )
+        {
+            my $pname = join '.', @name;
+            $cnt{ $pname } ++;
+            pop @name;
+        }
+    }
+
+    my $cnt = keys %tree;
+    my ( $match ) = sort{ length($b) <=> length($a) } grep { $cnt{$_} eq $cnt } keys %cnt;
+
+    return $tree unless $match;
+
+    $match .= '.';
+
+    my @subtree;
+    my $mlen = length $match;
+
+    for my $name ( keys %tree )
+    {
+        my $subname = substr $name, $mlen;
+        push @subtree, $subname;
+    }
+
+    return sprintf "$match\{%s\}", join ',', sort @subtree;
+}
+
 1;
