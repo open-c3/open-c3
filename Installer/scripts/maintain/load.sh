82f6f4a52a003b950d32f7a8608daba652cc2617
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-11-27 15:44:45 +0800

    maintain load.sh时先打上maintain标签

--- Installer/scripts/maintain/load.sh
+++ Installer/scripts/maintain/load.sh
@@ -8,6 +8,13 @@ if [ "X$version" == "X" ];then
     exit
 fi
 
+touch /data/open-c3/Connector/c3.maintain
+touch /data/open-c3/Connector/c3.maintain.flow
+touch /data/open-c3/Connector/c3.maintain.mon
+
+echo sleep 90 sec ...
+sleep 90
+
 ./load-1-base.sh
 ./load-2-db.sh $version
 ./load-3-log.sh
