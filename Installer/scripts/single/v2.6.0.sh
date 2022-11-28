c6ba44fee916f2c85b6f83627c6d4290e45e7b7a
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-11-28 20:11:11 +0800

    添加工具对数据进行加密解密

--- Installer/scripts/single/v2.6.0.sh
+++ Installer/scripts/single/v2.6.0.sh
@@ -59,3 +59,5 @@ mkdir -p /data/open-c3-data/cache/point
 mkdir -p /data/open-c3-data/cache/usertree
 
 docker exec -i openc3-server yum install mysql -y
+
+docker exec -i openc3-server /data/Software/mydan/perl/bin/cpan install Crypt::RC4::XS
