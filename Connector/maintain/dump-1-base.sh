ca9bf8e4490713504a6d67cb99e0024f5241ba06
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-11-28 15:56:48 +0800

    备份和恢复过程中处理CMDB账号、grafana和prometheus配置

--- Connector/maintain/dump-1-base.sh
+++ Connector/maintain/dump-1-base.sh
@@ -8,17 +8,20 @@ fi
 mkdir -p $DIST
 cd $DIST || exit
 
-mkdir -p data/auth
+mkdir -p data/auth data/account data/private data/grafana-data data/prometheus/config
 
+DPATH=/data/Software/mydan
 if [ "X$OPEN_C3_NAME" == "X" ];then
-    cp /data/open-c3/Connector/config.ini/current        data/
-else
-    cp /data/Software/mydan/Connector/config.ini/current data/
+    DPATH=/data/open-c3
 fi
 
-cp /data/open-c3-data/sysctl.conf             data/
+cp        $DPATH/Connector/config.ini/current   data/
+rsync -av $DPATH/AGENT/device/conf/account/     data/account/
+rsync -av $DPATH/prometheus/config/             data/prometheus/config/
 
-cp -r /data/open-c3-data/auth/* data/auth/
+cp /data/open-c3-data/sysctl.conf               data/
 
-mkdir -p data/private
-rsync -av /data/open-c3-data/private/ data/private/
+rsync -av /data/open-c3-data/auth/              data/auth/
+rsync -av /data/open-c3-data/private/           data/private/
+
+cp /data/open-c3-data/grafana-data/grafana.db   data/grafana-data/
