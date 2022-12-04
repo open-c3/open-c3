c73d6a018ba767c0248b4bf6b763ad18ee1cf442
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-12-04 11:02:58 +0800

    监控ACK页面中,支持直接操作alertmanager中的silence

--- AGENT/lib/api/monitor/ack.pm
+++ AGENT/lib/api/monitor/ack.pm
@@ -18,14 +18,16 @@ get '/monitor/ack/:uuid' => sub {
 
     my $uuid = substr( $param->{uuid}, 0, 12 );
 
-    my @col = qw( labels fingerprint caseuuid );
+    my @col = qw( id labels fingerprint caseuuid );
     my $r = eval{ 
         $api::mysql->query( 
             sprintf( "select %s from openc3_monitor_ack_table where ackuuid='$uuid'", join( ',', @col)), \@col )};
 
     my ( $acked, $time, @res ) = ( 0, time );
+    my $amackid = 0;
     for my $x ( @$r )
     {
+        $amackid = $x->{id};
         map{
             my @x = split /=/, $_, 2;
             push @res, +{ name => $x[0], value => $x[1] }
@@ -36,14 +38,16 @@ get '/monitor/ack/:uuid' => sub {
         $acked = 1 if @$xx > 0;
     }
 
-    return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => \@res, acked => $acked };
+    my $info = `amtool --alertmanager.url=http://openc3-alertmanager:9093 silence`;
+    my $amacked = $info =~ m#by-c3-ack-\($amackid\)# ? 1 : 0;
+    return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => \@res, acked => $acked, amacked => $amacked };
 };
 
 post '/monitor/ack/:uuid' => sub {
     my $param = params();
     my $error = Format->new( 
         uuid => qr/^[a-zA-Z0-9]+$/, 1,
-        ctrl => [ 'in', 'ack', 'ackcase' ], 1,
+        ctrl => [ 'in', 'ack', 'ackcase', 'ackam' ], 1,
     )->check( %$param );
 
     return  +{ stat => $JSON::false, info => "check format fail $error" } if $error;
@@ -59,6 +63,20 @@ post '/monitor/ack/:uuid' => sub {
         if( $ctrl eq 'ackcase' )
         {
             $api::mysql->execute( "insert into openc3_monitor_ack_active ( uuid,treeid,edit_user,expire ) select `caseuuid`,treeid,'$user','$time' from openc3_monitor_ack_table  where ackuuid='$uuid'" );
+        }
+        elsif( $ctrl eq 'ackam' )
+        {
+            my $x = $api::mysql->query( "select labels,id from openc3_monitor_ack_table where ackuuid='$uuid'" );
+            die "nofind your ackuuid" unless $x && @$x > 0;
+            my @x;
+            for( split /,/, $x->[0][0] )
+            {
+                next if $_ =~ /'/;
+                my @xx = split /=/, $_, 2;
+                push @x, "$1=$xx[1]" if $xx[0] =~ /^labels\.(.+)$/;
+            }
+            
+            system( sprintf "c3mc-mon-alertmanager-silence -c 'by-c3-ack-($x->[0][1])' -u '$user' %s", join ' ', map{ "'$_'" }@x  ) if @x;
         }
         else
         {
