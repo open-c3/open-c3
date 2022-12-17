c00db0afaf6f6d98fd49d2206041ae35440a485c
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-12-17 23:57:16 +0800

    添加pkg镜像，用于携带需要提前编译的文件

---
+++ Installer/C3/pkg/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -ex
+
+VERSION=$1
+if [ "X$VERSION" == "X" ];then
+    VERSION=$(date +%y%m%d)
+fi
+echo VERSION:$VERSION
+
+cd /data/open-c3/Installer/C3/pkg || exit
+
+rm -rf tempdata
+mkdir  tempdata
+
+# trouble-ticketing
+bash -c "cd /data/open-c3/Connector/tt/trouble-ticketing && ./build.sh"
+mkdir -p tempdata/open-c3/Connector/tt/trouble-ticketing
+cp /data/open-c3/Connector/tt/trouble-ticketing/trouble-ticketing tempdata/open-c3/Connector/tt/trouble-ticketing/
+chmod +x tempdata/open-c3/Connector/tt/trouble-ticketing/trouble-ticketing
+
+#jumpserver
+bash -c "cd /data/open-c3/Connector/bl/sync/jumpserver && ./build.sh"
+mkdir -p tempdata/open-c3/Connector/bl/sync/jumpserver
+cp /data/open-c3/Connector/bl/sync/jumpserver/jumpserver tempdata/open-c3/Connector/bl/sync/jumpserver/
+chmod +x tempdata/open-c3/Connector/bl/sync/jumpserver/jumpserver
+
+# baseinfo
+git log                > tempdata/git.log
+git branch | grep '^*' > tempdata/git.branch
+
+docker build . -t openc3/pkg:$VERSION --no-cache
---
+++ Installer/C3/pkg/extract.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+docker run -v /data/open-c3:/data/open-c3 openc3/pkg:2212171
