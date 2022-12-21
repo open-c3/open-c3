ab5661665dd6c1aabc841a9b279358d0abf496d4
leowu <wupengcn301@gmail.com>
2022-12-21 22:44:12 +0800

    修复执行脚本时提示无法找到脚本的错误

--- Connector/tt/trouble-ticketing/dev.sh
+++ Connector/tt/trouble-ticketing/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-./build.sh
+/data/open-c3/Connector/tt/trouble-ticketing/build.sh
 docker  exec -it openc3-server bash -c "/data/Software/mydan/Connector/tt/trouble-ticketing/tdev.sh"
