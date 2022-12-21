bdbc67f8fc48561d96811b763f1d0542b731487d
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-12-21 23:22:13 +0800

    优化当前告警页展示

--- c3-front/src/app/pages/allalerts/allalerts.controller.js
+++ c3-front/src/app/pages/allalerts/allalerts.controller.js
@@ -10,18 +10,22 @@
         vm.seftime = genericService.seftime
 
         vm.siteaddr = window.location.protocol + '//' + window.location.host;
+
         vm.reload = function () {
-            vm.loadover = false;
+            vm.reloadA();
+            vm.reloadB();
+        };
+        vm.reloadA = function () {
+            vm.loadAover = false;
             $http.get('/api/agent/monitor/alert/0?siteaddr=' + vm.siteaddr).success(function(data){
                 if (data.stat){
                     vm.dataTable = new ngTableParams({count:25}, {counts:[],data:data.data});
-                    vm.loadover = true;
+                    vm.loadAover = true;
                 }else {
                     swal({ title:'获取列表失败', text: data.info, type:'error' });
                 }
             });
         };
-        vm.reload();
 
         vm.tottbind = {};
         vm.reloadB = function () {
@@ -35,7 +39,7 @@
                 }
             });
         };
-        vm.reloadB();
+        vm.reload();
 
         vm.getinstancename = function( labels ) {
             var name = labels['instance'];
@@ -58,23 +62,39 @@
             return name;
         };
 
+        vm.loadover = true;
         vm.tott = function(d){
-            vm.loadover = false;
-            $http.post("/api/agent/monitor/alert/tott/0", d  ).success(function(data){
-                if(data.stat == true)
-                {
-                   vm.loadover = true;
-                   vm.cancel();
-                   swal({ title:'提交成功', text: data.info, type:'success' });
-                } else {
-                   swal({ title:'提交失败', text: data.info, type:'error' });
-                }
+            swal({
+                title: "提交工单",
+                text: '监控告警转工单',
+                type: "info",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                cancelButtonText: "取消",
+                confirmButtonText: "确定",
+                closeOnConfirm: true
+
+            }, function(){
+                vm.loadover = false;
+                $http.post("/api/agent/monitor/alert/tott/0", d  ).success(function(data){
+                    if(data.stat == true)
+                    {
+                       vm.loadover = true;
+                       vm.reload();
+                       swal({ title:'提交成功', text: data.info, type:'success' });
+                    } else {
+                       swal({ title:'提交失败', text: data.info, type:'error' });
+                    }
+                });
+
             });
         };
 
         vm.openTT = function (uuid, caseuuid) {
+            vm.loadover = false;
             $http.get('/api/agent/monitor/alert/gotocase/0?uuid=' + uuid + '&caseuuid=' + caseuuid ).success(function(data){
                 if (data.stat){
+                    vm.loadover = true;
                     window.open(data.data, '_blank')
                 }else {
                     swal({ title:'获取工单地址失败', text: data.info, type:'error' });
