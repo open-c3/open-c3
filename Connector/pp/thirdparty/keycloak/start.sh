36db4c00c42791639e792d016a3d2278e162baf3
lijinfeng2011 <lijinfeng2011@gmail.com>
2022-12-23 23:48:13 +0800

    keycloak服务通过主服务进行中转，避免keycloak禁用的情况lua启动失败

--- Connector/pp/thirdparty/keycloak/start.sh
+++ Connector/pp/thirdparty/keycloak/start.sh
@@ -10,3 +10,6 @@ docker run \
   -e PROXY_ADDRESS_FORWARDING=true \
   --name openc3-keycloak \
   openc3-keycloak
+
+docker exec openc3-server cp /data/Software/mydan/c3-front/nginxconf/open-c3.keycloak.conf-on /etc/nginx/conf.d/open-c3.keycloak.conf
+docker exec openc3-server nginx -s reload
--- Connector/pp/thirdparty/keycloak/stop.sh
+++ Connector/pp/thirdparty/keycloak/stop.sh
@@ -2,3 +2,6 @@
 
 docker stop openc3-keycloak
 docker rm   openc3-keycloak
+
+docker exec openc3-server cp /data/Software/mydan/c3-front/nginxconf/open-c3.keycloak.conf-off /etc/nginx/conf.d/open-c3.keycloak.conf
+docker exec openc3-server nginx -s reload
