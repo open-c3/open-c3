25930b817d5cb6fabfac3a6dc1573053207b2d82
leowu <wupengcn301@gmail.com>
2022-12-29 18:27:57 +0800

    发起bpm tt时ext_tt从系统参数读取

--- Connector/pp/bpm/action/tt/t.sh
+++ Connector/pp/bpm/action/tt/t.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "{\"content\":\"wfe\",\"title\":\"域名申请与解析\",\"ext_tt\":\"0\"}" | ./c3mc-bpm-action-tt 
+# submit_user可以不用传，默认为 sys@app
+
+echo "{\"content\":\"wfe\",\"title\":\"域名申请与解析\", \"submit_user\": \"someone@gmail.com\"}" | ./c3mc-bpm-action-tt 
+
+echo "{\"content\":\"wfe\",\"title\":\"域名申请与解析\"}" | ./c3mc-bpm-action-tt
