0e97ec6c5d1ffe4b76b2d3700e0354b71c631701
leowu <wupengcn301@gmail.com>
2023-01-10 11:14:50 +0800

    修改腾讯云clb工单配置及选项数据脚本

--- Connector/pp/bpm/action/qcloud-clb/option/t.sh
+++ Connector/pp/bpm/action/qcloud-clb/option/t.sh
@@ -6,5 +6,11 @@ c3mc-display-cloud-account-list qcloud
 echo '{"account": "openc3test"}' | c3mc-qcloud-cvm-describe-regions | c3mc-bpm-display-field-values Region,RegionName,RegionState
 # 查询项目列表
 echo '{"account": "openc3test", "region": "ap-beijing"}' | c3mc-qcloud-cvm-describe-projects | c3mc-bpm-display-field-values ProjectId,Name
+# 查询vpc列表
+echo '{"account": "openc3test", "region": "ap-beijing"}' | c3mc-qcloud-cvm-describe-vpcs | c3mc-bpm-display-field-values VpcId,VpcName
+# 查询子网列表
+echo '{"account": "openc3test", "region": "ap-beijing", "vpc_id": "vpc-wefwefwef", "zone": "ap-beijing-4"}' | c3mc-qcloud-cvm-describe-subnets | c3mc-bpm-display-field-values SubnetId,SubnetName,AvailableIpAddressCount
 # 查询cvm列表
-echo '{"account": "openc3test", "region": "ap-beijing"}' | c3mc-qcloud-clb-describe-cvm-list | c3mc-bpm-display-field-values InstanceId,InstanceName,PrivateIp,PublicIp
+echo '{"account": "openc3test", "region": "ap-beijing", "subnet_id": "xxxxxx"}' | c3mc-qcloud-clb-describe-cvm-list | c3mc-bpm-display-field-values InstanceId,InstanceName,PrivateIp,PublicIp
+# 查询证书列表
+echo '{"account": "openc3test"}' | c3mc-qcloud-clb-describe-cert-list | c3mc-bpm-display-field-values CertificateId,Domain
