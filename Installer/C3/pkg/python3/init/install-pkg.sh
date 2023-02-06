3891510e1a13fb8119679f212cc2ad6986ce1dd0
leowu <wupengcn301@gmail.com>
2023-02-06 19:30:02 +0800

    添加ibm虚拟机和裸金属服务器的同步

--- Installer/C3/pkg/python3/init/install-pkg.sh
+++ Installer/C3/pkg/python3/init/install-pkg.sh
@@ -10,4 +10,5 @@ set -e
 /data/Software/mydan/python3/bin/pip3 install -r /app/init/cloud-huawei-requrements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 /data/Software/mydan/python3/bin/pip3 install -r /app/init/cloud-qcloud-requrements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 /data/Software/mydan/python3/bin/pip3 install -r /app/init/cloud-ksyun-requrements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+/data/Software/mydan/python3/bin/pip3 install -r /app/init/cloud-ibm-requrements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 /data/Software/mydan/python3/bin/pip3 install -r /app/init/pip3-common-requrements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
