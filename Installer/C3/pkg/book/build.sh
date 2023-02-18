7998c032d21c4b4c9571b9afd68bf9eea302039b
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-02-18 15:05:40 +0800

    pkg book build每次都进行一次完整的下载

--- Installer/C3/pkg/book/build.sh
+++ Installer/C3/pkg/book/build.sh
@@ -3,10 +3,8 @@ set -ex
 
 cd /data/open-c3/Installer/C3/pkg/book || exit
 
-if [ ! -d book ];then
-    git clone https://github.com/open-c3/open-c3.github.io book
-fi
-bash -c "cd book && git pull";
+rm -rf book
+git clone https://github.com/open-c3/open-c3.github.io book
 tar -zcf book.tar.gz book --exclude .git
 
 mkdir -p _tempdata/open-c3/Connector/pkg
