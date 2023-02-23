c8fa1c6e603b36766c4805f480d88c45d18c80ca
leowu <wupengcn301@gmail.com>
2023-02-23 16:12:54 +0800

    修改创建腾讯云clb工单，添加健康检查字段，并优化字段展示脚本

--- Connector/pp/bpm/common_cmd/t.sh
+++ Connector/pp/bpm/common_cmd/t.sh
@@ -3,3 +3,6 @@ set -e
 
 echo '{"name":"wfww","sex":"man"}' | ./c3mc-bpm-display-field-values name,sex
 echo '{"name":"wfww","sex":"man"}' | ./c3mc-bpm-display-field-values "姓名是: {name}, 性别是: {sex}"
+echo '{"name":"wfww","vpc": {"id": "xxx"}}' | ./c3mc-bpm-display-field-values "姓名是: {name}, 性别是: {sex}, vpc.id为: {vpc[id]}"
+# 子字段支持无限层级
+echo '{"name":"wfww","vpc": {"id": {"index": 2}}}' | ./c3mc-bpm-display-field-values "姓名是: {name}, vpc.id.index为: {vpc[id][index]}"
