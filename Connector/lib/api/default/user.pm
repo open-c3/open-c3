4ba51d1103ea94db1dbde786afe5b743e219e23d
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-04-25 21:23:54 +0800

    u - 登录页面添加用户和IP访问频率限制

--- Connector/lib/api/default/user.pm
+++ Connector/lib/api/default/user.pm
@@ -227,8 +227,33 @@ any '/default/user/login' => sub {
         unless defined $user & defined $pass;
 
     $pass = encode_base64( $pass );
+
+    my $ip = '0.0.0.0';
+    my $time = time;
+
+    for( qw( HTTP_X_FORWARDED_FOR HTTP_X_REAL_IP REMOTE_ADDR ) )
+    {
+        my $x = request->env->{$_};
+        if( $x && $x =~ /^\s*(\d+\.\d+\.\d+\.\d+)\b/ )
+        {
+            $ip = $1;
+            last;
+        }
+    }
+
+    my $tt = time - 300;
+    my $pwErr = eval{ $api::mysql->query( "select count(*) from `openc3_connector_user_login_audit` where ip='$ip' and  action='pwErr' and t>'$tt'" ) };
+    return +{ stat => $JSON::false, info => $@ } if $@;
+    return +{ stat => $JSON::false, info => "Your IP access is too frequent, please try again later" } unless $pwErr && $pwErr->[0][0] < 50;
+
+    $pwErr = eval{ $api::mysql->query( "select count(*) from `openc3_connector_user_login_audit` where user='$user' and  action='pwErr' and t>'$tt'" ) };
+    return +{ stat => $JSON::false, info => $@ } if $@;
+    return +{ stat => $JSON::false, info => "Your account access is too frequent, please try again later" } unless $pwErr && $pwErr->[0][0] < 5;
+
+
     my $x = `c3mc-login --user '$user' --pass '$pass'`;
     chomp $x;
+
     if( $x eq 'ok' )
     {
         my @chars = ( "A" .. "Z", "a" .. "z", 0 .. 9 );
@@ -245,18 +270,6 @@ any '/default/user/login' => sub {
 
         set_cookie( $api::cookiekey => $keys, http_only => 0, expires => time + 8 * 3600, %domain );
 
-        my $ip = '0.0.0.0';
-        my $time = time;
-
-        for( qw( HTTP_X_FORWARDED_FOR HTTP_X_REAL_IP REMOTE_ADDR ) )
-        {
-            my $x = request->env->{$_};
-            if( $x && $x =~ /^\s*(\d+\.\d+\.\d+\.\d+)\b/ )
-            {
-                $ip = $1;
-                last;
-            }
-        }
         my $uuid = Digest::MD5->new->add($pass)->hexdigest;
 
         eval{ $api::mysql->execute( "insert into openc3_connector_user_login_audit( `user`,`uuid`,`action`,`ip`,`t` ) values('$user','$uuid','login','$ip','$time')" ); };
@@ -275,6 +288,8 @@ any '/default/user/login' => sub {
     }
     else
     {
+        eval{ $api::mysql->execute( "insert into openc3_connector_user_login_audit( `user`,`uuid`,`action`,`ip`,`t` ) values('$user','','pwErr','$ip','$time')" ); };
+        return +{ stat => $JSON::false, info => $@ } if $@;
         return +{ stat => $JSON::false, info => "Error. " . $x };
     }
 
