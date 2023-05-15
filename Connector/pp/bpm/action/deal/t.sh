ce913ef345b4b3a3e20695e99184b926592c758b
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-05-15 18:53:27 +0800

    BPM deal插件，发送邮件和短信通知

--- Connector/pp/bpm/action/deal/t.sh
+++ Connector/pp/bpm/action/deal/t.sh
@@ -1,1 +1,1 @@
-echo '{"_step_uuid_":"1","dealer":"open-c3"}' | C3BPMUUID=BPM202304300959014659 ./c3mc-bpm-action-deal
+echo '{"_step_uuid_":"1","submitter":"open-c3","caseurl": "http://127.0.0.1/#/bpm/0/BPM202304300959014659" }' | C3BPMUUID=BPM202304300959014659 ./c3mc-bpm-action-deal
