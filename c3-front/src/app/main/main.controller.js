ef1fe98cce47a0f826526c206c936989834b0d59
guo-zi-xin <1992059868@qq.com>
2023-05-15 16:33:03 +0800

    全局页面添加搜索按钮

--- c3-front/src/app/main/main.controller.js
+++ c3-front/src/app/main/main.controller.js
@@ -6,11 +6,40 @@
         .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController($rootScope) {
+    function MainController($rootScope, $uibModal, $state) {
         $rootScope.back = function() {
             $window.history.back();
         };
+        var vm = this;
+        vm.treeid = $state.params.treeid;
+        vm.modalIsOpen = false
+        vm.isShowSearch = true
+        var modalInstance
 
+        vm.handleClick = function () {
+          modalInstance = $uibModal.open({
+            templateUrl: 'app/components/globalsearch/globalsearch.html',
+            controller: 'GlobalSearchPageController',
+            controllerAs: 'globalsearch',
+            backdrop: 'static',
+            size: 'lg',
+            keyboard: false,
+            bindToController: true,
+            resolve: {
+              treeid: function () { return vm.treeid },
+            }
+          });
+          vm.modalIsOpen = true
+          modalInstance.result.then(function(result) {
+            console.log('Modal closed with result:', result);
+          },function (reason) {
+            vm.modalIsOpen = false
+          });
+        }
+
+        vm.handleModalIsOpen = function () {
+          vm.modalIsOpen = modalInstance && modalInstance.closed !== true;
+        }
     }
 
 })();
