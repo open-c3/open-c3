219821bcfd390a51e5e3d2f2fbefcac66e33afea
guo-zi-xin <1992059868@qq.com>
2023-05-25 17:40:59 +0800

    bpm添加定时任务列表

---
+++ c3-front/src/app/pages/history/timetask/timetask.controller.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  angular
+      .module('openc3')
+      .controller('HistoryTimeTaskController', HistoryTimeTaskController);
+
+  function HistoryTimeTaskController($filter, $state, $http, $uibModal, $scope, ngTableParams) {
+
+      var vm = this;
+      vm.treeid = $state.params.treeid;
+      $scope.nowTime = $filter('date')(new Date, "yyyy-MM-dd");
+
+      vm.reload = function () {
+        $http.get('/api/job/bpm/crontask').success(function (data) {
+          if (data.stat) {
+            vm.data_Table = new ngTableParams({count:10}, {counts:[],data:data.data.reverse()});
+          }else {
+            swal('获取列表失败', data.info, 'error');
+          }
+        }).error(function (err) {
+          swal('获取列表失败', err, 'error');
+        })
+      };
+
+      vm.reload();
+  }
+
+})();
