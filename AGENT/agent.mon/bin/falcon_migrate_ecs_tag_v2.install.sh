caf852e25765e86acd8f3a884b7328acaec88ddf
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-05-30 15:56:42 +0800

    f - 修复falcon_migrate_ecs_tag_v2安装脚本

--- AGENT/agent.mon/bin/falcon_migrate_ecs_tag_v2.install.sh
+++ AGENT/agent.mon/bin/falcon_migrate_ecs_tag_v2.install.sh
@@ -5,5 +5,5 @@ ECSM=`docker ps|grep amazonaws.com/liveme-falcon-micadvisor-open|wc -l`;
 echo ECSM:$ECSM
 
 if [ "X$ECSM" == "X1" ];then
-    cp /opt/mydan/dan/agent.mon/bin/falcon_migrate_ecs_tag_v2.crontab  /etc/cron.d/open-c3/falcon_migrate_ecs_tag_v2.crontab
+    cp /opt/mydan/dan/agent.mon/bin/falcon_migrate_ecs_tag_v2.crontab  /etc/cron.d/open-c3-falcon_migrate_ecs_tag_v2.crontab
 fi
