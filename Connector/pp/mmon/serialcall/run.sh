2d9d880f98127e1bd40a69af7680363230342ff0
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-07-20 11:54:45 +0800

    修改监控连环call的日志形式

--- Connector/pp/mmon/serialcall/run.sh
+++ Connector/pp/mmon/serialcall/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/data/Software/mydan/Connector/pp/mmon/serialcall/run 2>&1 | c3mc-base-log-addtimemin > /data/open-c3-data/serialcall.log 2>&1
+/data/Software/mydan/Connector/pp/mmon/serialcall/run 2>&1 | c3mc-base-log-addtime >> /data/open-c3-data/serialcall.log 2>&1
