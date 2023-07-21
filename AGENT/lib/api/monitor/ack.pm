0f6ac9ff6b021d29e23b5a6a538be54ecae990e9
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-07-21 19:03:14 +0800

    优化监控ack接口

--- AGENT/lib/api/monitor/ack.pm
+++ AGENT/lib/api/monitor/ack.pm
@@ -246,7 +246,6 @@ get '/monitor/ack/:uuid' => sub {
 
     $acked{tott} = $r && @$r ? 1 : 0;
 
-    my $u = (split /\//, $user )[0];
     my $acksc = eval{ $api::mysql->query( "select count(*) from openc3_monitor_serialcall_data where user='$u'" ) };
     return +{ stat => $JSON::false, info => $@ } if $@;
     $acked{acksc} = $acksc->[0][0] == 0 ? 1 : 0;
