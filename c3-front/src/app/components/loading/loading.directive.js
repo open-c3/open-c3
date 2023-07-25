2bc34ff1340f11c466d4359523b770b5301d33e2
guo-zi-xin <1992059868@qq.com>
2023-07-25 18:17:10 +0800

    loading加载动画添加遮罩层

--- c3-front/src/app/components/loading/loading.directive.js
+++ c3-front/src/app/components/loading/loading.directive.js
@@ -8,7 +8,10 @@
     function loading() {
         var directive = {
             restrict: 'E',
-            templateUrl: 'app/components/loading/loading.html'
+            templateUrl: 'app/components/loading/loading.html',
+            scope: {
+              isCoverage: '=' // 添加 isCoverage 属性到指令的作用域中
+            }
         };
         return directive;
     }
