6b7666e942291b93b293e55561e5638d67a34864
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-08-01 18:06:51 +0800

    google登录出错时，把错误信息跳转回原始登录页

--- Connector/lib/api/loginext/google.pm
+++ Connector/lib/api/loginext/google.pm
@@ -30,9 +30,16 @@ post '/loginext/google' => sub {
     my $error = Format->new(
         credential => qr/^[a-zA-Z0-9\@_\.\-_]+$/, 1,
     )->check( %$param );
-    return +{ stat => $JSON::false, info => "check format fail $error" } if $error;
 
-    return +{ stat => $JSON::false, info => "Google login not on,Please have the admin open it" } unless $api::loginext::data{'google'}{'on'};
+    my $makeErrUrl = sub
+    {
+        my $err = shift @_;
+        return sprintf "%s/loginext/google.html?toastrError=$err&%s", $api::loginext::data{'google'}{'domain'}, join '&', map{ "$_=$api::loginext::data{'google'}{$_}" }keys %{$api::loginext::data{'google'}};
+    };
+
+    return redirect &$makeErrUrl( "check format fail $error" ) if $error;
+
+    return redirect &$makeErrUrl( "Google login not on,Please have the admin open it" ) unless $api::loginext::data{'google'}{'on'};
 
     my ( $domain, $err ) = @$param{qw( domain )};
 
@@ -51,20 +58,20 @@ post '/loginext/google' => sub {
 
     my $tt = time - 300;
     my $pwErr = eval{ $api::mysql->query( "select count(*) from `openc3_connector_user_login_audit` where ip='$ip' and  action='pwErr' and t>'$tt'" ) };
-    return +{ stat => $JSON::false, info => $@ } if $@;
-    return +{ stat => $JSON::false, info => "Your IP access is too frequent, please try again later" } unless $pwErr && $pwErr->[0][0] < 50;
+    return redirect &$makeErrUrl( "Err: $@" ) if $@;
+    return redirect &$makeErrUrl( "Your IP access is too frequent, please try again later" ) unless $pwErr && $pwErr->[0][0] < 50;
 
     my $ua = LWP::UserAgent->new;
     $ua->timeout(10);
 
     my $res = $ua->get( "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=$param->{credential}" );
-    return +{ stat => $JSON::false, info => sprintf "call www.googleapis.com fail, code: %s", $res->code } unless $res->is_success;
+    return redirect &$makeErrUrl( sprintf "call www.googleapis.com fail, code: %s", $res->code ) unless $res->is_success;
 
     my $dat = eval{ JSON::from_json( $res->decoded_content ) };
-    return +{ stat => $JSON::false, info => sprintf "call www.googleapis.com no json" } if $@;
+    return redirect &$makeErrUrl( "call www.googleapis.com no json" ) if $@;
 
     my $user = $dat->{email};
-    return +{ stat => $JSON::false, info => sprintf "call www.googleapis.com nofind user" } unless $user;
+    return redirect &$makeErrUrl( "call www.googleapis.com nofind user" ) unless $user;
 
     my @chars = ( "A" .. "Z", "a" .. "z", 0 .. 9 );
     my $keys = join("", @chars[ map { rand @chars } ( 1 .. 64 ) ]);
@@ -75,7 +82,7 @@ post '/loginext/google' => sub {
         my $x  = $api::mysql->query( "select id from openc3_connector_userinfo where name='$user'" );
         $api::mysql->execute( "insert into openc3_connector_userinfo(name) value('$user')" ) unless @$x;
     };
-    return +{ stat => $JSON::false, info => $@ } if $@;
+    return redirect &$makeErrUrl( "Err: $@" ) if $@;
 
     eval{ $api::mysql->execute( sprintf "update openc3_connector_userinfo set expire=%d,sid='%s' where name='%s'", time + 7 * 86400, $keys, $user ); };
     return +{ stat => $JSON::false, info => $@ } if $@;
@@ -90,7 +97,7 @@ post '/loginext/google' => sub {
     set_cookie( $api::cookiekey => $keys, http_only => 0, expires => time + 8 * 3600, %domain );
 
     eval{ $api::mysql->execute( "insert into openc3_connector_user_login_audit( `user`,`uuid`,`action`,`ip`,`t` ) values('$user','$uuid','login','$ip','$time')" ); };
-    return +{ stat => $JSON::false, info => $@ } if $@;
+    return redirect &$makeErrUrl( "Err: $@" ) if $@;
 
     redirect $api::loginext::data{'google'}{'domain'};
 };
