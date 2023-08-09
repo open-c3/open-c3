3600e45f1c9a821c45b346b060b7034b38e73938
wupeng <>
2023-08-09 16:55:41 +0800

    复制堡垒机用户权限的脚本增加更多日志信息

--- Connector/lib/pp/c3mc_utils.py
+++ Connector/lib/pp/c3mc_utils.py
@@ -283,6 +283,9 @@ def safe_run_command(cmd_parts):
     Args:
         cmd_parts (list): 数组格式的命令。例如要运行命令 "ls -alh"，则传递 ["ls", "-alh"]
     """
+
+    print("debug, safe_run_command: ", " ".join(cmd_parts), file=sys.stderr)
+
     output = subprocess.run(cmd_parts, capture_output=True, text=True)
     if output.returncode != 0:
         raise RuntimeError(output.stderr)
