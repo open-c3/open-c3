428e07e04cd593dd8dda3971ac905cdfe0ef09ba
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-09-07 22:48:23 +0800

    添加ali pay监控指标采集

--- Connector/pp/mmon/crontab/ali-pai/get.py
+++ Connector/pp/mmon/crontab/ali-pai/get.py
@@ -7,6 +7,7 @@ from alibabacloud_tea_openapi.models import Config
 from alibabacloud_paistudio20220112.client import Client
 import alibabacloud_paistudio20220112.models as models
 
+import os
 import sys
 from datetime import datetime, timedelta
 
@@ -18,6 +19,8 @@ access_key_secret=sys.argv[4]
 region_id=sys.argv[5]
 
 clusters = sys.argv[6:]
+
+ali_pai_metrics_value = os.environ.get("ALI_PAI_Metrics")
 
 config = Config(
     access_key_id=access_key_id,
@@ -34,14 +37,31 @@ def calculate_dates(input_date, days):
     future_day = input_date + timedelta(days=days-1)
     return previous_day.strftime("%Y-%m-%d"), future_day.strftime("%Y-%m-%d")
 
-date1, date2 = calculate_dates(startdate, int(daycount))
-start_time=date1+"T16:00:00.000Z"
-end_time=date2+"T15:59:59.000Z"
+def getTime():
+    current_time = datetime.now()
+
+    current_time -= timedelta(hours=8)
+
+    time_40_minutes_ago = current_time - timedelta(minutes=40)
+    time_10_minutes_ago = current_time - timedelta(minutes=10)
+
+    time_format = "%Y-%m-%dT%H:%M:00.000Z"
+    time_string_40_minutes_ago = time_40_minutes_ago.strftime(time_format)
+    time_string_10_minutes_ago = time_10_minutes_ago.strftime(time_format)
+    return time_string_40_minutes_ago, time_string_10_minutes_ago
+
+
+if int(daycount) > 0:
+    date1, date2 = calculate_dates(startdate, int(daycount))
+    start_time=date1+"T16:00:00.000Z"
+    end_time=date2+"T15:59:59.000Z"
+else:
+    start_time, end_time = getTime()
 
 instantRequest = models.GetNodeMetricsRequest(verbose=True, start_time=start_time, end_time=end_time)
 
 timelist = []
-def getData(resourceClient,instantRequest,metric_type,resource_group_id):
+def getData(resourceClient,instantRequest,metric_type,resource_group_id,clustername):
     instantResponse=resourceClient.get_node_metrics(
         resource_group_id=resource_group_id,
         metric_type=metric_type, 
@@ -65,6 +85,9 @@ def getData(resourceClient,instantRequest,metric_type,resource_group_id):
             if not cluster.get(metric.time):
                 cluster[metric.time] = []
  
+            if ali_pai_metrics_value == "1":
+                print( "ali_pai_%s{cluster=\"%s\",node=\"%s\"} %s %s" % ( metric_type, clustername, nodeID, metric.value, metric.time ) )
+
             res[nodeID].append(metric.value)
             cluster[metric.time].append(metric.value)
 
@@ -76,8 +99,8 @@ def getData(resourceClient,instantRequest,metric_type,resource_group_id):
         clusterstatistics.append(avgv)
     return res, clusterstatistics
 
-def getRes(resourceClient,instantRequest,metric_type,resource_group_id):
-    nodedata, clusterdata  = getData(resourceClient,instantRequest, metric_type, resource_group_id)
+def getRes(resourceClient,instantRequest,metric_type,resource_group_id,clustername):
+    nodedata, clusterdata  = getData(resourceClient,instantRequest, metric_type, resource_group_id,clustername)
 
     metric_type_alias = metric_type
     if metric_type == 'GpuUsageRate':
@@ -103,17 +126,17 @@ def getRes(resourceClient,instantRequest,metric_type,resource_group_id):
     return noderes, clusterres
 
 
-def getClusterRes(resourceClient,instantRequest,resource_group_id):
+def getClusterRes(resourceClient,instantRequest,resource_group_id,clustername):
     clusterdata = {}
     nodedata = {}
     for metricname in [ 'GpuUsageRate', 'GpuMemoryUsageRate' ]:
-        nodedatarrr, clusterdatarrr  = getRes(resourceClient,instantRequest, metricname, resource_group_id)
-        clusterdata.update( clusterdatarrr )
-        for node in nodedatarrr.keys():
+        nodedatares, clusterdatares  = getRes(resourceClient,instantRequest, metricname, resource_group_id,clustername)
+        clusterdata.update( clusterdatares )
+        for node in nodedatares.keys():
             if nodedata.get(node):
-                nodedata[node].update( nodedatarrr[node] )
+                nodedata[node].update( nodedatares[node] )
             else:
-                nodedata[node] = nodedatarrr[node]
+                nodedata[node] = nodedatares[node]
 
     return nodedata, clusterdata
 
@@ -132,31 +155,34 @@ def printTable(data):
         print(" | ".join(formatted_row))
 
 def printCluster(resourceClient,instantRequest,resource_group_id, clustername):
-    nodedatarrr, clusterdatarrr  = getClusterRes(resourceClient,instantRequest, resource_group_id)
+    nodedatares, clusterdatares  = getClusterRes(resourceClient,instantRequest, resource_group_id,clustername)
 
     data = []
 
-    title = list( clusterdatarrr.keys() )
+    title = list( clusterdatares.keys() )
     data.append( [ clustername ] + title )
 
     cluster = [ "Cluster" ]
     for i in title:
-        cluster.append( clusterdatarrr[i] )
+        cluster.append( clusterdatares[i] )
     data.append(cluster)
 
 
-    node = nodedatarrr.keys()
+    node = nodedatares.keys()
     for nodename in node:
         nodelist = [ nodename ]
         for i in title:
-            nodelist.append( nodedatarrr[nodename][i] )
+            nodelist.append( nodedatares[nodename][i] )
         data.append(nodelist)
 
-    printTable(data)
+    if ali_pai_metrics_value != "1":
+        printTable(data)
 
 for l in clusters:
     x = l.split(":")
     printCluster(resourceClient,instantRequest, x[0], x[1])
-    print("\n")
+    if ali_pai_metrics_value != "1":
+        print("\n")
 
-print( "Time: {} ~ {}".format( datetime.fromtimestamp(min( timelist )), datetime.fromtimestamp(max(timelist ))))
+if ali_pai_metrics_value != "1":
+    print( "Time: {} ~ {}".format( datetime.fromtimestamp(min( timelist )), datetime.fromtimestamp(max(timelist ))))
