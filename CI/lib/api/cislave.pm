2cc3afae0dac0f1d02bb75fb754f3cae86ffbfcc
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-09-23 18:32:37 +0800

    添加ci slave列表接口

---
+++ CI/lib/api/cislave.pm
@@ -0,0 +1,44 @@
+package api::cislave;
+use Dancer ':syntax';
+use Dancer qw(cookie);
+use Encode qw(encode);
+
+use JSON qw();
+use POSIX;
+use MIME::Base64;
+use api;
+use Format;
+
+my $cislave;
+
+BEGIN{
+    my $f = "/data/Software/mydan/CI/cislave/conf/slave.yml";
+    if( -f $f )
+    {
+        my $x = eval{ YAML::XS::LoadFile $f };
+        die "load $f fail: $@" if $@;
+        $cislave = $x if $x && ref $x eq 'ARRAY';
+    }
+    else
+    {
+        $cislave = [];
+    }
+};
+=pod
+
+获取CI中可用的slave列表。
+
+=cut
+
+get '/cislave/node' => sub {
+    my $pmscheck = api::pmscheck( 'openc3_ci_read', 0 ); return $pmscheck if $pmscheck;
+
+    my @node = qw( master );
+    for( @$cislave )
+    {
+        push @node, $_->{host};
+    }
+    return $@ ? +{ stat => $JSON::false, info => $@ } : +{ stat => $JSON::true, data => \@node };
+};
+
+true;
