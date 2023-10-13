fe2d20428a9c777858480879c25d943c12926bfa
lijinfeng2011 <lijinfeng2011@gmail.com>
2023-10-13 18:34:58 +0800

    优化CMDB中lb后端信息的展示

--- CI/lib/api/c3mc/cloud/control/lbinfo.pm
+++ CI/lib/api/c3mc/cloud/control/lbinfo.pm
@@ -24,14 +24,15 @@ get '/c3mc/cloud/control/lbinfo/get/:type/:subtype/:uuid' => sub {
     my $error = Format->new( 
         type    => qr/^[a-zA-Z0-9][a-zA-Z0-9\-]+$/,  1,
         subtype => qr/^[a-zA-Z0-9][a-zA-Z0-9\-]+$/,  1,
-        uuid    => qr/^[a-zA-Z0-9][a-zA-Z0-9\-_]+$/, 1,
+        uuid    => qr/^[a-zA-Z0-9][a-zA-Z0-9\-_\.]+$/, 1,
+        table   => qr/^\d+$/, 0,
     )->check( %$param );
 
     return  +{ stat => $JSON::false, info => "check format fail $error" } if $error;
  
     my $pmscheck = api::pmscheck( 'openc3_ci_read', 0 ); return $pmscheck if $pmscheck;
 
-    my $filter = +{};
+    my $filter = +{ table => $param->{table} ? 1 : 0 };
 
     my $cmd = "c3mc-cloud-control --type '$param->{type}' --subtype '$param->{subtype}' --uuid '$param->{uuid}' --ctrl get-backend-servers x 2>&1";
     my $handle = 'cloud_lbinfo_get';
@@ -43,20 +44,31 @@ $handle{cloud_lbinfo_get} = sub
 {
     my ( $x, $status, $filter ) = @_;
     return +{ stat => $JSON::false, info => $x } if $status;
-    my $d = eval{ YAML::XS::Load $x };
-    return +{ stat => $JSON::false, info => "load data fail: $@" } if $@;
 
-    my %title;
-    for my $dd ( @$d )
+    if( $filter->{table} )
     {
-        for my $k ( keys %$dd )
-        {
-            $title{$k} ++;
-            $dd->{$k} = join( ',', @{$dd->{$k}} ) if ref $dd->{$k} eq 'ARRAY';
-        }
-    }
+        my $d = eval{ YAML::XS::Load $x };
+        return +{ stat => $JSON::false, info => "load data fail: $@" } if $@;
 
-    return +{ stat => $JSON::true, debug => $x, data => $d, title => [ sort keys %title ] };
+        my %title;
+        for my $dd ( @$d )
+        {
+            for my $k ( keys %$dd )
+            {
+                $title{$k} ++;
+                $dd->{$k} = join( ',', @{$dd->{$k}} ) if ref $dd->{$k} eq 'ARRAY';
+            }
+        }
+
+        return +{ stat => $JSON::true, data => $d, title => [ sort keys %title ] };
+    }
+    else
+    {
+        my $d = eval{ YAML::XS::Dump YAML::XS::Load $x };
+        return +{ stat => $JSON::false, info => "load data fail: $@" } if $@;
+
+        return +{ stat => $JSON::true, data => $d };
+    }
 };
 
 true;
