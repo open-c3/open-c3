d6b19a249082591df8ab40bc6ec4e16eb20f5011
李锦丰 <lijinfeng2011@gmail.com>
2024-07-13 09:41:37 +0800

    Revert "添加nginx配置文件检索功能"

--- Connector/pp/service-analysis/nginx/golang-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-docker run --rm -i \
-  --workdir /code \
-  -v /data/open-c3/Connector/pp/service-analysis/nginx:/code \
-  -v /data/open-c3/Connector/pp/service-analysis/nginx/tmp:/go golang:1.22 \
-   go build -o c3mc-service-analysis-get-nginx-conf main.go
