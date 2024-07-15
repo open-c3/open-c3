8a3dd9ae98854dda8c6107a0b36e9a15d7531ab0
lijinfeng2011 <lijinfeng2011@gmail.com>
2024-07-15 18:33:51 +0800

    修改nginx配置文件解析程序的编译脚本,更换临时路径

--- Connector/pp/service-analysis/nginx/golang-build.sh
+++ Connector/pp/service-analysis/nginx/golang-build.sh
@@ -4,5 +4,5 @@ set -e
 docker run --rm -i \
   --workdir /code \
   -v /data/open-c3/Connector/pp/service-analysis/nginx:/code \
-  -v /data/open-c3/Connector/pp/service-analysis/nginx/tmp:/go golang:1.22 \
+  -v /data/open-c3-data/golang-build/tmp/service-analysis/nginx:/go golang:1.22 \
    go build -o c3mc-service-analysis-get-nginx-conf main.go
