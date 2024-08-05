69f4d6add221d655ddcabb575bcfef43ef97c9a8
dengshenglong <1039277923@qq.com>
2024-08-05 21:43:04 +0800

    针对华为云资源可用区等信息的获取逻辑进行更新,以及一些bug修复

--- Connector/lib/pp/c3mc_cloud_huawei_flavors.py
+++ Connector/lib/pp/c3mc_cloud_huawei_flavors.py
@@ -2,32 +2,41 @@
 # -*- coding: utf-8 -*-
 
 import json
-
 from huaweicloudsdkcore.auth.credentials import BasicCredentials
 from huaweicloudsdkecs.v2.region.ecs_region import EcsRegion
 from huaweicloudsdkecs.v2 import *
-
+from huaweicloudsdkcore.exceptions import exceptions
 
 class Flavors:
     def __init__(self, access_id, access_key, project_id, region):
         self.access_id = access_id
         self.access_key = access_key
         self.region = region
-        if project_id in [None, "None"]:
-            self.project_id = None
-        else:
-            self.project_id = project_id.strip()
+        self.project_id = project_id.strip() if project_id not in [None, "None"] else None
         self.client = self.create_client()
 
     def create_client(self):
-        credentials = BasicCredentials(self.access_id, self.access_key, self.project_id)
-
-        return EcsClient.new_builder() \
-        .with_credentials(credentials) \
-        .with_region(EcsRegion.value_of(self.region)) \
-        .build()
+        try:
+            credentials = BasicCredentials(self.access_id, self.access_key, self.project_id)
+            return EcsClient.new_builder() \
+                .with_credentials(credentials) \
+                .with_region(EcsRegion.value_of(self.region)) \
+                .build()
+        except exceptions.ClientRequestException as e:
+            print(f"Warning: Error creating client: {e}")
+            return None
 
     def list_flavors(self):
-        request = ListFlavorsRequest()
-        response = self.client.list_flavors(request)
-        return json.loads(str(response))["flavors"]
+        if self.client is None:
+            print("Warning: Client not initialized. Skipping flavor list.")
+            return []
+
+        try:
+            request = ListFlavorsRequest()
+            response = self.client.list_flavors(request)
+            return json.loads(str(response))["flavors"]
+        except exceptions.ClientRequestException as e:
+            print(f"Warning: Error listing flavors: {e}")
+            if e.error_code == "APIGW.0802":
+                print("The IAM user does not have permission in this region. Skipping flavor list.")
+            return []
