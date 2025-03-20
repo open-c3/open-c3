c1abce934620c5c12513843e93b803cc54e5130e
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-03-21 07:33:45 +0800

    谷歌评论回复交互优化，提交过程显示提交中，避免重复提交

--- c3-front/src/app/pages/googleplayreview/reply.controller.js
+++ c3-front/src/app/pages/googleplayreview/reply.controller.js
@@ -13,57 +13,24 @@
         vm.data = data;
         vm.postdata = {};
 
-vm.replycontent = '';
+        vm.replycontent = '';
         vm.cancel = function(){ $uibModalInstance.dismiss()};
 
+        vm.showsubmit = 1;
         vm.add = function(){
+            vm.showsubmit = 0;
             $http.post('/api/ci/v2/c3mc/googleplay/review/reply', { "callback": data.callback, "review_id":data.review_id, "text":vm.replycontent} ).success(function(data){
                 if(data.stat == true) {
                     vm.cancel();
                     reload();
-                } else { swal({ title: "添加失败!", text: data.info, type:'error' }); }
+                } else {
+                    vm.showsubmit = 1;
+                    swal({ title: "添加失败!", text: data.info, type:'error' });
+                }
 
             });
         };
 
-var id=0;
-        vm.reload = function () {
-            vm.loadover = false
-            $http.get('/api/agent/cloudmon/' + id ).then(
-                function successCallback(response) {
-                    if (response.data.stat){
-                        vm.postdata = response.data.data
-                        vm.loadover = true
-                    }else {
-                        swal('获取信息失败', response.data.info, 'error' );
-                    }
-                },
-                function errorCallback (response ){
-                    swal('获取信息失败', response.status, 'error' );
-                });
-        };
-
-        if( id > 0 )
-        {
-            vm.reload();
-        }
-
-        vm.exporter = [];
-        vm.reloadexporter = function () {
-            $http.get('/api/agent/cloudmon/exporter' ).then(
-                function successCallback(response) {
-                    if (response.data.stat){
-                        vm.exporter = response.data.data
-                    }else {
-                        swal('获取信息失败', response.data.info, 'error' );
-                    }
-                },
-                function errorCallback (response ){
-                    swal('获取信息失败', response.status, 'error' );
-                });
-        };
-
-        vm.reloadexporter();
     }
 })();
 
