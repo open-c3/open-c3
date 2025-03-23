ca7705caefeea4c019e18e61cdb2590d7cbd4ac3
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-03-23 08:25:30 +0800

    堡垒机代理接口，改用golang:1.23

--- Connector/bl/proxy/build.sh
+++ Connector/bl/proxy/build.sh
@@ -4,5 +4,5 @@ set -e
 docker run --rm -i \
   --workdir /code \
   -v /data/open-c3/Connector/bl/proxy:/code \
-  -v /data/open-c3/Connector/bl/proxy/tmp:/go golang:1.17.10 \
+  -v /data/open-c3/Connector/bl/proxy/tmp:/go golang:1.23 \
    go build -o c3-proxy main.go
