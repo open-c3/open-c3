b2976fbc60d9dec812bc61120d67104a1af27e69
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-06-09 17:28:10 +0800

    u - CMDB支持vmware资源的同步

--- c3-front/src/app/pages/device/menu/menu.controller.js
+++ c3-front/src/app/pages/device/menu/menu.controller.js
@@ -17,7 +17,7 @@
         vm.selectedtimemachine = 'curr';
         vm.timemachine = [];
 
-        vm.cloudList = ['qcloud','aws', 'aliyun','ksyun','google','huawei','ibm', 'idc','openstack','ucloud']
+        vm.cloudList = ['qcloud','aws', 'aliyun','ksyun','google','huawei','ibm', 'idc','openstack','ucloud', 'vmware']
         vm.totalResources = {
           compute: 0,
           networking: 0,
