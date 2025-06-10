28acbba2549390e882d3be0d7a40598603a1d03c
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-06-10 10:50:50 +0800

    补充perl模块描述，自动化系统会自动编译新perl上传

--- Installer/C3/pkg/perl/make.sh
+++ Installer/C3/pkg/perl/make.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -ex
 
-/data/Software/mydan/perl/bin/cpan install Net::LDAP Crypt::RC4::XS
+/data/Software/mydan/perl/bin/cpan install Net::LDAP
+/data/Software/mydan/perl/bin/cpan install Crypt::RC4::XS
 /data/Software/mydan/perl/bin/cpan install Email::Sender::Simple
 /data/Software/mydan/perl/bin/cpan install Email::Sender::Transport::SMTPS
 /data/Software/mydan/perl/bin/cpan install Auth::GoogleAuth
 /data/Software/mydan/perl/bin/cpan install MIME::Words
 /data/Software/mydan/perl/bin/cpan install AnyEvent::Ping
+/data/Software/mydan/perl/bin/cpan install Net::DNS::Dig
+/data/Software/mydan/perl/bin/cpan install AnyEvent::HTTPD::Router
+/data/Software/mydan/perl/bin/cpan install AnyEvent::HTTPD::CookiePatch
 
 rm -rf /data/Software/mydan/perl/man
 rm -rf /root/.cpan
