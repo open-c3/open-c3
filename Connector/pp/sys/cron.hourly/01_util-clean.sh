1b6e3431f04b42e9029fa4bbbd86397cb77a96d4
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-06-12 10:18:26 +0800

    定时清理CMDB中临时数据

--- Connector/pp/sys/cron.hourly/01_util-clean.sh
+++ Connector/pp/sys/cron.hourly/01_util-clean.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo skip
 #docker images|grep '<none>'|awk '{print $3}'|xargs -i{} docker rmi {}
 #docker ps -a|grep openc3ci|grep -v 'Exited (0)'|awk '{print $1}'|xargs -i{} docker rm {}
 #docker system prune
+
+# 这里可以优化一下，同步的时候不要同步它，免得删除一次。
+# 但是历史遗留，需要先把历史的都删除了
+find /data/Software/mydan/AGENT/device/conf/account.temp  -mtime +1 -exec rm -rf {} \;
+rm -rf /data/open-c3-data/device/curr/conf/account.temp
+rm -rf /data/open-c3-data/device/timemachine/*/conf/account.temp
