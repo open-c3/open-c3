5fd775f003c6644752889039de08ce5878b7f41b
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-06-17 22:55:03 +0800

    更新mongodb采集安装脚本，安装最新编译的镜像

--- AGENT/scripts/installMongodbQuery.sh
+++ AGENT/scripts/installMongodbQuery.sh
@@ -2,7 +2,7 @@
 
 curl -L $OPEN_C3_ADDR/api/scripts/installQueryInit.sh | bash
 
-IMAGE=openc3/mongodb-query:o2307271
+IMAGE=openc3/mongodb-query:o2506171
 VPATH=mongodb-exporter-v3
 NAME=openc3-mongodb-query
 
@@ -12,7 +12,7 @@ docker stop $NAME 2>/dev/null
 docker rm   $NAME 2>/dev/null
 
 docker run -d \
-  -v /bin/docker:/bin/docker \
+  -v /bin/docker:/bin/docker_v1.0.0 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /data/open-c3-data/$VPATH:/data/open-c3-data/$VPATH \
   -p 65115:65115 \
