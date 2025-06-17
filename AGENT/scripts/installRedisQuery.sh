627534a074684ff8770166dbd514e990a11cddc1
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-06-17 18:05:01 +0800

    更新redis采集安装脚本，安装最新编译的镜像

--- AGENT/scripts/installRedisQuery.sh
+++ AGENT/scripts/installRedisQuery.sh
@@ -2,7 +2,7 @@
 
 curl -L $OPEN_C3_ADDR/api/scripts/installQueryInit.sh | bash
 
-IMAGE=openc3/redis-query:e2307271
+IMAGE=openc3/redis-query:e2506171
 VPATH=redis-exporter-v3
 NAME=openc3-redis-query
 
@@ -12,7 +12,7 @@ docker stop $NAME 2>/dev/null
 docker rm   $NAME 2>/dev/null
 
 docker run -d \
-  -v /bin/docker:/bin/docker \
+  -v /bin/docker:/bin/docker_v1.0.0 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v /data/open-c3-data/$VPATH:/data/open-c3-data/$VPATH \
   -p 65114:65114 \
