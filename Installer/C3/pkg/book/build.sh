cee458ab364551bed03789ae09ad4d9b2c90d437
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-07-10 05:05:43 +0000

    book构建使用新版book内容

--- Installer/C3/pkg/book/build.sh
+++ Installer/C3/pkg/book/build.sh
@@ -12,6 +12,8 @@ cd $C3BASEPATH/open-c3/Installer/C3/pkg/book || exit
 
 rm -rf book
 git clone https://github.com/open-c3/open-c3.github.io book
+bash -c "cd book; bash fix-assets.sh"
+bash -c "cd book; bash fix-assets-book.sh"
 tar --exclude=.git -zcf book.tar.gz book
 
 mkdir -p _tempdata/open-c3/Connector/pkg
